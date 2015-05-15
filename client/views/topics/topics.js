c88e1d85ad3b6b1d14bcc480b273cd730cc5042e
jackkav <jackkav@gmail.com>
2015-05-15 17:17:06 +0800

    added a treeview for topics

---
+++ client/views/topics/topics.js
@@ -0,0 +1,23 @@
+var data = [
+    {
+        label: '<a href="test">Acoustics</a>', id: 1,
+        children: [
+            { label: '<a href="test">Acoustic instrumentation</a>', id: 2 },
+            { label: '<a href="test"> Acoustic modeling</a>', id: 3 }
+        ]
+    },
+    {
+        label: '<a href="test">Astronomy and astrophysics</a>', id: 4,
+        children: [
+            { label: '<a href="test">Astrophysics</a>', id: 5 }
+        ]
+    }
+];
+Template.Topics.rendered = function () {
+$('#topicTree').tree({
+    data: data,
+    autoOpen: false,
+    autoEscape: false
+});
+};
+
