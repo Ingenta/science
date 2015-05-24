aa25680153a9264a6d745706c0be38b2eab0eb3c
jackkav <jackkav@gmail.com>
2015-05-24 19:55:47 +0800

    link to publisher homepage

--- client/views/publications/show_publisher.js
+++ client/views/publications/show_publisher.js
@@ -1,17 +1,7 @@
-Session.setDefault('thisPublisher', 0);
 Template.PublicationList.helpers({
     publications: function () {
       return Publications.find();
   }
 });
-Template.PublisherWebsite.helpers({
-    thisPublisher: function () {
-        return Session.get('thisPublisher');
-    }
-});
 Template.PublisherWebsite.rendered = function() {
-    // var thisPublisherUrl = Router.current().params.urlname;
-    // var thisPub = this.Publishers.findOne({urlname: thisPublisherUrl});
-    // console.log(thisPub.name);
-    // Session.set("thisPublisher", this.Publishers.findOne().name);
 };
