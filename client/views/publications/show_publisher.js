cea9778699f199719bbffe9c1709a6c2419930da
jackkav <jackkav@gmail.com>
2015-05-28 23:06:34 +0800

    fixed sidebar layout bug

--- client/views/publications/show_publisher.js
+++ client/views/publications/show_publisher.js
@@ -16,6 +16,15 @@ Template.ShowPublisher.helpers({
   }
 });
 
+Template.ShowPublisher.helpers({
+  getImage: function (pictureId) {
+    var noPicture ="http://sbiapps.sitesell.com/sitebuilder/sitedesigner/resource/basic_white_nce/image-files/thumbnail1.jpg"
+    if(pictureId===undefined)
+      return noPicture;
+    return Images.findOne({_id: pictureId}).url();
+  }
+});
+
 AutoForm.addHooks(['addPublicationForm'], {
   onSuccess: function () {
     FlashMessages.sendSuccess("Success!", { hideDelay: 5000 });
