06ed82bdb639ce5ef3ad60c40908bf9f36773d08
jackkav <jackkav@gmail.com>
2015-05-28 12:03:33 +0800

    simplified publications

--- both/collections/publications.js
+++ both/collections/publications.js
@@ -5,10 +5,6 @@ PublicationsSchema  = new SimpleSchema({
         type: String,
         unique: true
     },
-    chinesetitle: {
-        type: String,
-        unique: true
-    },
     urlname: {
         type: String,
         unique: true
@@ -20,18 +16,7 @@ PublicationsSchema  = new SimpleSchema({
             rows: 2
         }
     },
-    chinesedescription: {
-        type: String,
-        optional: true,
-        autoform: {
-            rows: 2
-        }
-    },
     firstletter:{
-        type: String,
-        max: 1
-    },
-    chinesefirstletter:{
         type: String,
         max: 1
     },
@@ -47,15 +32,3 @@ Meteor.startup(function() {
     PublicationsSchema .i18n("schemas.publications");
     Publications.attachSchema(PublicationsSchema);
 });
-
-this.Publications.userCanInsert = function(userId, doc) {
-	return true;
-}
-
-this.Publications.userCanUpdate = function(userId, doc) {
-	return true;
-}
-
-this.Publications.userCanRemove = function(userId, doc) {
-	return true;
-}
