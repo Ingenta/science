45c86479463a776d97d687a41774c9b677481280
xiancui <451763649@qq.com>
2015-05-29 16:58:17 +0800

    publishers update

--- client/views/publications/publications.js
+++ client/views/publications/publications.js
@@ -1,32 +1,35 @@
 Template.FilterList.helpers({
-		publishers: function () {
-		  return Publishers.find();
-		}, 
-		publications: function () {
-		  return Publications.find();
-		},
-		filteredPublications: function () {
-			var pubId = Session.get('filterPublisher');
-			if(pubId===undefined){
-				return Publications.find();
-			}
-		  return Publications.find({publisher:pubId});
-		},
-		count: function (url) {
-		  return Publications.find({publisher:url}).count();
-		},
-		getImage: function (pictureId) {
-		  var noPicture ="http://sbiapps.sitesell.com/sitebuilder/sitedesigner/resource/basic_white_nce/image-files/thumbnail1.jpg"
-		  if(pictureId===undefined)
-		  return noPicture;
-		  return Images.findOne({_id: pictureId}).url();
-	    }
+	publishers: function () {	
+		var pubId = Session.get('filterPublisher');
+		if(pubId===undefined){
+			return Publishers.find();
+		}
+	  return Publishers.find({_id:pubId});
+	},
+	publications: function () {
+		var pubId = Session.get('filterPublisher');
+		if(pubId===undefined){
+			return Publications.find();
+		}
+	  return Publications.find({publisher:pubId});
+	},
+	count: function (id) {
+	  return Publications.find({publisher:id}).count();
+	},
+	getImage: function (pictureId) {
+	  var noPicture ="http://sbiapps.sitesell.com/sitebuilder/sitedesigner/resource/basic_white_nce/image-files/thumbnail1.jpg"
+	  if(pictureId===undefined)
+	  return noPicture;
+	  return Images.findOne({_id: pictureId}).url();
+	}
 
 });
   Template.FilterList.events({
     'click .filterButton': function (event) {
 		var f = $(event.currentTarget).find('.filterId').html();
-		console.log(f);
       Session.set('filterPublisher', f);
     }
   });
+Template.FilterList.onRendered(function () {
+	Session.set('filterPublisher', undefined);
+});
