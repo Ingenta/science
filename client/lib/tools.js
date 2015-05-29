38816e3502c08fb01bf0db14d8096361586abbcc
baidan4855 <baidan4855@qq.com>
2015-05-29 17:35:05 +0800

    GBK->UTF-8

--- client/lib/tools.js
+++ client/lib/tools.js
@@ -1,9 +1,9 @@
 /**
- * Created by 蒋凯 on 2015/5/29.
+ * Created by 钂嬪嚡 on 2015/5/29.
  */
 (function (window) {
     var PTool = {
-        /**根据图片ID获取图片*/
+        /**鏍规嵁鍥剧墖ID鑾峰彇鍥剧墖*/
         getImage:function(pictureId) {
             var noPicture = "http://sbiapps.sitesell.com/sitebuilder/sitedesigner/resource/basic_white_nce/image-files/thumbnail1.jpg";
             return (Images && pictureId && Images.findOne({_id: pictureId}).url()) || noPicture;
