571959ef1e58e62a572c9300603f3af4199fb712
arthuryyx <arthuryyx@hotmail.com>
2015-06-03 18:02:54 +0800

    reformatting

--- both/collections/publications.js
+++ both/collections/publications.js
@@ -1,42 +1,41 @@
 this.Publications = new Meteor.Collection("publications");
 
-PublicationsSchema  = new SimpleSchema({
+PublicationsSchema = new SimpleSchema({
     title: {
         type: String,
         unique: true
     },
-    firstletter:{
+    firstletter: {
         type: String,
         max: 1
     },
     accessKey: {
-      type: String,
-  },
-  description: {
-    type: String,
-    optional: true,
-    autoform: {
-        rows: 4
+        type: String
+    },
+    description: {
+        type: String,
+        optional: true,
+        autoform: {
+            rows: 4
+        }
+    },
+    publisher: {
+        type: String
+    },
+    picture: {
+        type: String,
+        optional: true,
+        autoform: {
+            afFieldInput: {
+                type: 'fileUpload',
+                collection: 'Images',
+                accept: 'image/gif,image/jpeg,image/png,.gif,.jpeg,.jpg,.png'
+            }
+        },
+        label: 'Choose file'
     }
-},
-publisher:
-{
-    type: String
-},
-picture: {
-    type: String,
-    optional: true,
-    autoform: {
-      afFieldInput: {
-        type: 'fileUpload',
-        collection: 'Images',
-        accept: 'image/gif,image/jpeg,image/png,.gif,.jpeg,.jpg,.png'
-    }
-},
-label: 'Choose file'
-}
 });
-Meteor.startup(function() {
-    PublicationsSchema .i18n("schemas.publications");
+Meteor.startup(function () {
+    PublicationsSchema.i18n("schemas.publications");
     Publications.attachSchema(PublicationsSchema);
 });
