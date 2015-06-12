d2e093fea8064b87b4608b6a0ccbcf4bb72a19be
arthuryyx <arthuryyx@hotmail.com>
2015-06-12 09:57:49 +0800

    tabs checkbox localization

--- both/collections/publications.js
+++ both/collections/publications.js
@@ -47,23 +47,7 @@ PublicationsSchema = new SimpleSchema({
     tabSelections: {
         type: [String],
         autoform: {
-            type: "select-checkbox-inline",
-            options: function () {
-                return [
-                    {label : "Overview",  value: "Overview"},
-                    {label : "Browse", value: "Browse"},
-                    {label : "About", value: "About"},
-                    {label : "Editorial Board",  value: "Editorial Board"},
-                    {label : "News", value: "News"},
-                    {label : "Media", value: "Media"},
-                    {label : "Special Topics",  value: "Special Topics"},
-                    {label : "Collections", value: "Collections"},
-                    {label : "Accepted", value: "Accepted"},
-                    {label : "Online First",  value: "Online First"},
-                    {label : "MOOP", value: "MOOP"},
-                    {label : "Author", value: "Author"}
-                ];
-            }
+            type: "select-checkbox-inline"
         }
     }
 });
