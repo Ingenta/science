e254c3f3a1c450569cf6c55a6eabdded711246a8
baidan4855 <baidan4855@qq.com>
2015-06-15 16:49:34 +0800

    更新文档示例

--- packages/science-topics/client/views/topics.js
+++ packages/science-topics/client/views/topics.js
@@ -1,13 +1,35 @@
 Template.SingleTopic.events({
-   'click .fa': function (event) {
+    'click .fa': function (event) {
        var id=$(event.currentTarget).parent().attr('id');
        var es=Session.get("expandStatus"+id);
        Session.set("expandStatus"+id,!es);
        Session.set("parentId", id);
        event.stopPropagation();
-   }
+    }
 });
-
+Template.TopicList.events({
+    'keyup .refineSearch': function(event) {
+        $(".parentTopicList li,.parentTopicList hr").hide();
+        var term = $(event.currentTarget).val();
+        _.each($(".parentTopicList li"), function (item) {
+            var eachTopic = item.textContent.trim().toLowerCase();
+            if (eachTopic && eachTopic.indexOf(term.toLowerCase().trim()) > -1) {
+                recursionLi(item);
+                $(item).show();
+                $(item).next("hr").show();
+            }
+        });
+        function recursionLi(li){
+            var parentLi = $(li).parent().parent();
+            console.log(parentLi);
+            if(parentLi && parentLi.length && !parentLi.visibility){
+                $(parentLi).show();
+                $(parentLi).next("hr").show();
+                recursionLi(parentLi);
+            }
+        }
+    }
+});
 Template.TopicList.helpers({
     topics: function () {
         return  Topics.find({"parentId" : null});
@@ -27,6 +49,10 @@ Template.SingleTopic.helpers({
     },
     isExpand: function(id){
         return Session.get("expandStatus"+id) || false;
+    },
+    isParentExpand:function(id){
+        var topic=Topics.findOne({_id:id});
+        return Session.get("expandStatus" + topic.parentId);
     }
 });
 
