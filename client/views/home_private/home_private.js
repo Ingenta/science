4ba4d6af40a00d08ba5de956d6dfcb2a3f4cb3c7
liuqi0 <809270376@qq.com>
2015-06-15 18:05:30 +0800

    homepage news show

--- client/views/home_private/home_private.js
+++ client/views/home_private/home_private.js
@@ -1,43 +1,41 @@
-Template.HomePrivate.rendered = function() {
-
+Template.SingleNews.rendered = function() {
+	$('.NewsPicture img:gt(0)').hide();
 };
 
-Template.HomePrivate.events({
-
+Template.SingleNews.events({
+	'mouseenter .SingleNews':function(){
+		$('.NewsPicture img').hide();
+		$('.NewsPicture #'+this._id).show();
+	}
 });
-
-Template.HomePrivate.helpers({
-
-});
-
 
 Template.NewsList.helpers({
-  news: function () {
-    return News.find();
-  }
+	news: function () {
+		return News.find();
+	}
 });
 
 Template.deleteNewsModalForm.helpers({
-  getPrompt: function () {
-    return TAPi18n.__("Are you sure?");
-  }
+	getPrompt: function () {
+		return TAPi18n.__("Are you sure?");
+	}
 });
 
 Template.SingleNews.helpers({
-    hasNews: function (id) {
-        return  News.find({"news": id}).count()===0;
-    }
+	hasNews: function (id) {
+		return  News.find({"news": id}).count()===0;
+	}
 });
 
 AutoForm.addHooks(['addNewsModalForm'], {
-  onSuccess: function () {
-    $("#addNewsModal").modal('hide');
-    FlashMessages.sendSuccess("Success!", { hideDelay: 5000 });
-  }
+	onSuccess: function () {
+		$("#addNewsModal").modal('hide');
+		FlashMessages.sendSuccess("Success!", { hideDelay: 5000 });
+	}
 }, true);
 
 AutoForm.addHooks(['cmForm'], {
-  onSuccess: function () {
-    FlashMessages.sendSuccess("Success!", { hideDelay: 5000 });
-  }
+	onSuccess: function () {
+		FlashMessages.sendSuccess("Success!", { hideDelay: 5000 });
+	}
 }, true);
