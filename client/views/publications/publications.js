e93b4b60432f42a41d574c60431ec897ba632ae9
jackkav <jackkav@gmail.com>
2015-06-17 23:48:29 +0800

    refactored publications view

--- client/views/publications/publications.js
+++ client/views/publications/publications.js
@@ -1,3 +1,30 @@
+Template.onePublication.helpers({
+	getUrlToJournal: function (id, title) {
+		var name =Publishers.findOne({_id:id}).name
+		return "/publisher/"+name+"/journal/"+title;
+	}
+});
+Template.onePublisherInFilterList.helpers({
+	count: function (id) {
+		var first = Session.get('firstletter');
+		if(first===undefined)
+			return Publications.find({publisher:id}).count();
+		return Publications.find({publisher:id,firstletter:first}).count();
+	}
+});
+Template.PublicationsAlphabetBar.helpers({
+	totalPublicationResults: function () {
+		return Session.get('totalPublicationResults');
+
+	},
+	totalPluralPublicationResults: function () {
+		var total= Session.get('totalPublicationResults');
+		return pluralize(total, 'result');
+	},
+	letterInTheAlphabet: function () {
+		return "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z".split(',');
+	}
+});
 Template.FilterList.helpers({
 	publishers: function () {
 		var pubId = Session.get('filterPublisher');
@@ -7,7 +34,6 @@ Template.FilterList.helpers({
 		return Publishers.find({_id:pubId});
 	},
 	publications: function () {
-
 		var pubId = Session.get('filterPublisher');
 		var firstletter = Session.get('firstletter');
 		var q={};
@@ -17,32 +43,11 @@ Template.FilterList.helpers({
 		var pubs=myPubPagination.find(q, {itemsPerPage:10});
 		return pubs;
 	},
-	totalPublicationResults: function () {
-		return Session.get('totalPublicationResults');
-
-	},
-	totalPluralPublicationResults: function () {
-		var total= Session.get('totalPublicationResults');
-		return pluralize(total, 'result');
-	},
-	count: function (id) {
-		var first = Session.get('firstletter');
-		if(first===undefined)
-			return Publications.find({publisher:id}).count();
-		return Publications.find({publisher:id,firstletter:first}).count();
-	},
 	selectedPublisher: function(){
 		return Session.get('filterPublisher');
 	},
 	getPublisherNameById: function (id) {
 		return Publishers.findOne({_id:id}).name;
-	},
-	letterInTheAlphabet: function () {
-		return "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z".split(',');
-	},
-	getUrlToJournal: function (id, title) {
-		var name =Publishers.findOne({_id:id}).name
-		return "/publisher/"+name+"/journal/"+title;
 	}
 
 });
@@ -62,6 +67,9 @@ Template.FilterList.events({
 	},
 	'click .onup': function(event){
 		$(event.target).next("ul").toggle();
+	},
+	'click .resetAlphabetFilter': function(event){
+		Session.set('firstletter', undefined);
 	}
 });
 Template.FilterList.onRendered(function () {
