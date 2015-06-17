a43c6da04fc6303ce281810acfd2adfb10633961
baidan4855 <baidan4855@qq.com>
2015-06-17 14:24:55 +0800

    small change of more content by

--- client/views/publications/articles/authorPopButton.js
+++ client/views/publications/articles/authorPopButton.js
@@ -3,15 +3,15 @@ Template.authorPopButton.events({
         var ele=$(event.target);
         var name=Template.currentData().name;
         ele.text(name);
-        var html="<ul class='list-unstyled author-list-ul'>";
-        html+="<li><a href='/author/"+ name +"'>"+TAPi18n.__("inStation")+"</a></li>";
+        var html="<div class='author-pop'>";
+        html+="<div><a href='/author/"+ name +"'>"+TAPi18n.__("inStation")+"</a></div>";
         html+="<hr>";
-        html+="<li><a target='_blank' href='http://www.ncbi.nlm.nih.gov/pubmed?term="+name+"[Author]'>PubMed</a></li>";
+        html+="<div><a target='_blank' href='http://www.ncbi.nlm.nih.gov/pubmed?term="+name+"[Author]'>PubMed</a></div>";
         html+="<hr>";
-        html+="<li><a target='_blank' href='http://scholar.google.com/scholar?q=author:"+name+"'>Google Scholar</a></li>";
-        html+="</ul>";
+        html+="<div><a target='_blank' href='http://scholar.google.com/scholar?q=author:"+name+"'>Google Scholar</a></div>";
+        html+="</div>";
         ele.popover({
-            title:TAPi18n.__("moreContentBy")+"<br><span class='pop-author-name'>"+Template.currentData().name+"</span>",
+            title:TAPi18n.__("moreContentBy")+"<br><span class='author-pop-name'>"+Template.currentData().name+"</span>",
             content:html
         });
         ele.popover('show');
