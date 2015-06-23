48a091f5130df6b8ea16d396859eab92f0d6f066
arthuryyx <arthuryyx@hotmail.com>
2015-06-23 18:19:01 +0800

    authors on one Article page works

--- client/views/search/oneArticle.js
+++ client/views/search/oneArticle.js
@@ -18,5 +18,8 @@ Template.oneArticle.helpers({
     },
     journalName:function(id){
         return Publications.findOne({_id:id}).title;
+    },
+    getFullName:function(){
+        return this.surname + ' ' + this.given;
     }
 });
