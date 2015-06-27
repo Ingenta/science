aacfe7e8d4480a5b0faf05d76babcb217e5b944e
jackkav <jackkav@gmail.com>
2015-06-27 12:51:03 +0800

    added a test to confirm two p tags can be parsed without adding commas

--- packages/science-xml/science-xml-tests.js
+++ packages/science-xml/science-xml-tests.js
@@ -67,6 +67,17 @@ Tinytest.add('Given valid xml, and xpath, When abstract is not missing and has x
     test.equal(results.abstract, abstractResult);
 });
 
+Tinytest.add('Given valid xml, and xpath, When abstract is not missing, has 2 paragraphs and has xml inside, Should return abstract with xml and encoded tags', function (test) {
+    var abstractContent = "<p>Dana-Farber and Scripps Florida scientists have identified an alternative mechanism by which PPAR<sub>&#x3B3;</sub> agonists could exert their antidiabetic effects. The findings provide a rationale for a renewed look at compounds previously dismissed due to poor agonist activity but that now may offer therapeutic benefits with the potential for fewer side effects than marketed PPAR<sub>&#x3B3;</sub> agonists.</p>";
+    var abstractResult = "<p>Dana-Farber and Scripps Florida scientists have identified an alternative mechanism by which PPAR<sub>γ</sub> agonists could exert their antidiabetic effects. The findings provide a rationale for a renewed look at compounds previously dismissed due to poor agonist activity but that now may offer therapeutic benefits with the potential for fewer side effects than marketed PPAR<sub>γ</sub> agonists.</p>";
+    var testXml = "<book><author>Terry Pratchett<sub>esq.</sub></author><title>Witches abroad<sup>1</sup></title><abstract abstract-type='editor'>"+abstractContent+abstractContent+"</abstract></book>";
+    var xmlDom = new dom();
+    var results = {};
+    var doc = xmlDom.parseFromString(testXml);
+    results = ScienceXML.getAbstract(results, doc);
+    test.equal(results.abstract, abstractResult+abstractResult);
+});
+
 Tinytest.add('Given valid xml, and xpath, When issn is not missing, Should return issn', function (test) {
     var testXml = "<book><issn pub-type='ppub'>123-123123</issn><author>Terry Pratchett</author><article-title>Witches abroad</article-title></book>";
     var xmlDom = new dom();
