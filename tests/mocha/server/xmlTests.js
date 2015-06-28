048a6d93b469f8fc6abd244fe610914132eef07e
jackkav <jackkav@gmail.com>
2015-06-28 14:35:49 +0800

    fixed false positives

--- tests/mocha/server/xmlTests.js
+++ tests/mocha/server/xmlTests.js
@@ -1,28 +1,35 @@
+chai.should();
 if (!(typeof MochaWeb === 'undefined')) {
     MochaWeb.testOnly(function () {
         describe("Given an article xml", function () {
             describe("When parsing a simple field", function () {
                 it("should get the contents of a simple xpath", function () {
-                    var testXml = "<book><author>Terry Pratchet</author><article-title>Witches abroad</article-title></book>";
+                    var testXml = "<book><author>Terry Pratchett</author><article-title>Witches abroad</article-title></book>";
                     var doc = ScienceXML.xmlStringToXmlDoc(testXml);
-                    var title = ScienceXML.getSimpleValueByXPath("//author", doc);
-                    chai.assert("Terry Pratchett", title);
+                    var result = ScienceXML.getSimpleValueByXPath("//author", doc);
+                    result.should.equal("Terry Pratchett");
                 });
             });
             describe("When parsing a field with xml inside that we dont want", function () {
                 it("should get the contents of including inner xml without tags", function () {
-                    var testXml = "<book><author>Terry Pratchet<sub>esq.</sub></author><article-title>Witches abroad</article-title></book>";
+                    var testXml = "<book><author>Terry Pratchett<sub>esq.</sub></author><article-title>Witches abroad</article-title></book>";
                     var doc = ScienceXML.xmlStringToXmlDoc(testXml);
-                    var title = ScienceXML.getValueByXPathIgnoringXml("//author", doc);
-                    chai.assert("Terry Pratchettesq.", title);
+                    var result = ScienceXML.getValueByXPathIgnoringXml("//author", doc);
+                    result.should.equal("Terry Pratchettesq.");
                 });
             });
             describe("When parsing a field with xml inside that we want", function () {
                 it("should get the contents of including inner xml with tags", function () {
-                    var testXml = "<book><author>Terry Pratchet<sub>esq.</sub></author><article-title>Witches abroad</article-title></book>";
+                    var testXml = "<book><author>Terry Pratchett<sub>esq.</sub></author><article-title>Witches abroad</article-title></book>";
                     var doc = ScienceXML.xmlStringToXmlDoc(testXml);
-                    var title = ScienceXML.getValueByXPathIncludingXml("//author", doc);
-                    chai.assert("Terry Pratchett<sub>esq.</sub>.", title);
+                    var result = ScienceXML.getValueByXPathIncludingXml("//author", doc);
+                    result.should.equal("Terry Pratchett<sub>esq.</sub>");
+                });
+                it("should get the encode contents of including inner xml with tags", function () {
+                    var testXml = "<book><author>Terry Pratchett<sub>&#x3B3;</sub></author><article-title>Witches abroad</article-title></book>";
+                    var doc = ScienceXML.xmlStringToXmlDoc(testXml);
+                    var result = ScienceXML.getValueByXPathIncludingXml("//author", doc);
+                    result.should.equal("Terry Pratchett<sub>γ</sub>");
                 });
             });
         });
