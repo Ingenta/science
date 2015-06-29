d6472f7ff95b434b3adce150a80c63da6308494d
jackkav <jackkav@gmail.com>
2015-06-29 10:06:53 +0800

    removed tests

--- tests/mocha/server/xmlTests.js
+++ tests/mocha/server/xmlTests.js
@@ -28,18 +28,18 @@ if (!(typeof MochaWeb === 'undefined')) {
             });
             describe("When parsing a field with xml inside that we dont want", function () {
                 it("should get the contents of including inner xml without tags", function () {
-                    var testXml = "<book><author>Terry Pratchett<sub>esq.</sub></author><article-title>Witches abroad</article-title></book>";
+                    var testXml = "<book><author>Terry Pratchett<sub> esq.</sub></author><article-title>Witches abroad</article-title></book>";
                     var doc = ScienceXML.xmlStringToXmlDoc(testXml);
                     var result = ScienceXML.getValueByXPathIgnoringXml("//author", doc);
-                    result.should.equal("Terry Pratchettesq.");
+                    result.should.equal("Terry Pratchett esq.");
                 });
             });
             describe("When parsing a field with xml inside that we want", function () {
                 it("should get the contents of including inner xml with tags", function () {
-                    var testXml = "<book><author>Terry Pratchett<sub>esq.</sub></author><article-title>Witches abroad</article-title></book>";
+                    var testXml = "<book><author>Terry Pratchett<sub> esq.</sub></author><article-title>Witches abroad</article-title></book>";
                     var doc = ScienceXML.xmlStringToXmlDoc(testXml);
                     var result = ScienceXML.getValueByXPathIncludingXml("//author", doc);
-                    result.should.equal("Terry Pratchett<sub>esq.</sub>");
+                    result.should.equal("Terry Pratchett<sub> esq.</sub>");
                 });
                 it("should get the encode contents of including inner xml with tags", function () {
                     var testXml = "<book><author>Terry Pratchett<sub>&#x3B3;</sub></author></book>";
