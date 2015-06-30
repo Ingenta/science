123238dd575f4788d88b9d19816e02b4effad1b7
jackkav <jackkav@gmail.com>
2015-06-30 14:17:16 +0800

    refactored urlHelper

--- client/views/home_private/home_private.js
+++ client/views/home_private/home_private.js
@@ -27,13 +27,7 @@ Template.deleteNewsModalForm.helpers({
 Template.mostRecentUpload.helpers({
     newArticle: function () {
         return Articles.find({}, {sort: {createdAt: -1}, limit: 3});
-    },
-    urlToArticle: function (title) {
-        var article = Articles.findOne({title: title});
-        var publisherName = Publishers.findOne({_id: article.publisher}).name;
-        var journalName = Publications.findOne({_id: article.journalId}).title;
-        return "/publisher/" + publisherName + "/journal/" + journalName + "/article/" + title;
-    },
+    }
 });
 
 Template.SingleNews.helpers({
