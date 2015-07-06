77c58284093bfc8c08bc126fcc77d15e53e22b19
baidan4855 <baidan4855@qq.com>
2015-07-07 01:07:23 +0800

    delete the old access control codes add source code of package : accounts-base  account-password

--- client/views/router.js
+++ client/views/router.js
@@ -30,125 +30,15 @@ Meteor.subscribe('images');
 
 //Meteor.subscribe('articleViews');
 
-var publicRoutes = ["home_public", "login", "register", "forgot_password", "reset_password"];
-var privateRoutes = ["home_private", "admin", "admin.upload", "admin.users", "admin.users.details", "admin.users.insert", "admin.users.edit", "user_settings", "user_settings.profile", "user_settings.change_pass", "user_settings.update_information", "logout"];
-var zonelessRoutes = ["topics", "publishers", "publications", "collections"];
-
-var roleMap = [
-    {route: "admin", roles: ["admin", "orgadmin"]},
-    {route: "admin.users", roles: ["admin", "orgadmin"]},
-    {route: "admin.users.details", roles: ["admin", "orgadmin"]},
-    {route: "admin.users.insert", roles: ["admin"]},
-    {route: "admin.users.edit", roles: ["admin", "orgadmin"]},
-    {route: "user_settings", roles: ["user", "admin"]},
-    {route: "user_settings.profile", roles: ["user", "admin"]},
-    {route: "user_settings.change_pass", roles: ["user", "admin"]},
-    {route: "user_settings.update_information", roles: ["user", "admin"]}
-];
-
-this.firstGrantedRoute = function () {
-    var grantedRoute = "";
-    _.every(privateRoutes, function (route) {
-        if (routeGranted(route)) {
-            grantedRoute = route;
-            return false;
-        }
-        return true;
-    });
-
-    if (grantedRoute == "") {
-        if (routeGranted("home_private")) {
-            return "home_private";
-        } else {
-            return "home_public";
-        }
-    }
-
-    return grantedRoute;
-}
-
-// this function returns true if user is in role allowed to access given route
-this.routeGranted = function (routeName) {
-    if (!routeName) {
-        // route without name - enable access (?)
-        return true;
-    }
-
-    if (!roleMap || roleMap.length === 0) {
-        // this app don't have role map - enable access
-        return true;
-    }
-
-    var roleMapItem = _.find(roleMap, function (roleItem) {
-        return roleItem.route == routeName;
-    });
-    if (!roleMapItem) {
-        // page is not restricted
-        return true;
-    }
-
-    if (!Meteor.user() || !Meteor.user().roles) {
-        // user is not logged in
-        return false;
-    }
-
-    // this page is restricted to some role(s), check if user is in one of allowedRoles
-    var allowedRoles = roleMapItem.roles;
-    var granted = _.intersection(allowedRoles, Meteor.user().roles);
-    if (!granted || granted.length === 0) {
-        return false;
-    }
-
-    return true;
-};
-
-Router.ensureLogged = function () {
-    if (Meteor.userId() && (!Meteor.user() || !Meteor.user().roles)) {
-        return;
-    }
-
-    if (!Meteor.userId()) {
-        // user is not logged in - redirect to public home
-        this.redirect("home_public");
-    } else {
-        // user is logged in - check role
-        if (!routeGranted(this.route.getName())) {
-            // user is not in allowedRoles - redirect to private home
-            var redirectRoute = firstGrantedRoute();
-            this.redirect(redirectRoute);
-        } else {
-            this.next();
-        }
-    }
-};
-
-Router.ensureNotLogged = function () {
-    if (Meteor.userId() && (!Meteor.user() || !Meteor.user().roles)) {
-        return;
-    }
-
-    if (Meteor.userId()) {
-        var redirectRoute = firstGrantedRoute();
-        this.redirect(redirectRoute);
-    }
-    this.next();
-
-};
-
-
 Router.onBeforeAction(function () {
     // loading indicator here
     if (!this.ready()) {
         $("body").addClass("wait");
     } else {
         $("body").removeClass("wait");
+        this.next();
     }
-    this.next();
 });
-
-
-Router.onBeforeAction(Router.ensureNotLogged, {only: publicRoutes});
-Router.onBeforeAction(Router.ensureLogged, {only: privateRoutes});
 
 Router.map(function () {
 
