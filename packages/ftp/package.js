40156b512059c500cc0baf76d37ec75212cb3895
arthuryyx <arthuryyx@hotmail.com>
2015-07-10 13:41:40 +0800

    ftp package

---
+++ packages/ftp/package.js
@@ -0,0 +1,29 @@
+Package.describe({
+    name: 'arthuryyx:ftp',
+    version: '0.3.10',
+    // Brief, one-line summary of the package.
+    summary: 'Wrapper for npm ftp package',
+    // URL to the Git repository containing the source code for this package.
+    git: '',
+    // By default, Meteor will default to using README.md for documentation.
+    // To avoid submitting documentation, set this field to null.
+    documentation: 'README.md'
+});
+
+Npm.depends({
+    "ftp": "0.3.10",
+    "fs-extra": "0.22.1"
+});
+
+Package.onUse(function (api) {
+    api.versionsFrom('1.1.0.2');
+    api.addFiles('ftp.js', 'server');
+    api.export('FTP');
+    api.export('FSE');
+});
+
+Package.onTest(function (api) {
+    api.use('tinytest');
+    api.use('arthuryyx:ftp');
+    api.addFiles('ftp-tests.js', 'server');
+});
--- server/interface.js
+++ server/interface.js
@@ -3,12 +3,50 @@ Router.route('/api', function () {
     var res = this.response;
     res.write("query:\n");
     //req.query["aa"]
-    _.each(req.query,function(item){
-        res.write(item+"\n");
+    _.each(req.query, function (item) {
+        res.write(item + "\n");
     });
     res.write("form:\n");
-    _.each(req.body,function(item){
-        res.write(item+"\n");
+    _.each(req.body, function (item) {
+        res.write(item + "\n");
     });
     res.end();
+    callFtp();
 }, {where: 'server'});
+
+var callFtp = function () {
+    var fs = FSE;
+    var c = new FTP();
+
+    c.on('ready', function () {
+        c.size('1.4788933.zip', function (err, res) {
+            if (err) throw err;
+            var originalSize = res;
+            c.get('1.4788933.zip', function (err, stream) {
+                if (err) throw err;
+                stream.on('close', function () {
+//                    console.log('request finished downloading file');
+                    c.end();
+                    var downloadedSize = fs.statSync('E:\\xml\\1.4788933.zip').size;
+                    console.log(originalSize);
+                    console.log(downloadedSize)
+                    if (originalSize === downloadedSize)console.log("its not fucked");
+                    if (originalSize !== downloadedSize)console.log("its fucked");
+                });
+                stream.on('error', function (err) {
+                    console.log(err);
+                    throw  err;
+                });
+                stream.pipe(fs.createWriteStream('E:\\xml\\1.4788933.zip'));
+            });
+        });
+
+    });
+
+    c.connect(
+        {
+            user: "Ingenta",
+            password: "123"
+        }
+    );
+};
