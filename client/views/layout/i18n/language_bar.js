0c54ab471d37d2fb36f4f7d381a78989dc0eb218
baidan4855 <baidan4855@qq.com>
2015-07-13 00:42:07 +0800

    add a TAPi18n API named TAPi18n.addChangeHook

--- client/views/layout/i18n/language_bar.js
+++ client/views/layout/i18n/language_bar.js
@@ -2,10 +2,23 @@ Template.i18n_switch.events({
     // set language to context tag
     'click .tap-i18n-buttons a': function (event, tmpl) {
         var id = $(event.currentTarget).attr('id')
-        return TAPi18n.setLanguageAmplify(id)
+        return TAPi18n.setLanguageAmplify(id);
     }
 });
 TAPi18n._afterUILanguageChange = function () {
 //triggers on language switch
-    mo.setLocale(TAPi18n.getLanguage())
+    mo.setLocale(TAPi18n.getLanguage());
+    debugger
+    if(TAPi18n.afterChangeHook){
+        _.map(TAPi18n.afterChangeHook,function(hook,key){
+            hook && hook();
+        })
+    }
 };
+
+TAPi18n.addChangeHook=function(name, func){
+    if(!TAPi18n.afterChangeHook){
+        TAPi18n.afterChangeHook = {};
+    }
+    TAPi18n.afterChangeHook[name] = func;
+}
