46c5a127edb7957bfda1707f3bdcd34a613bc1de
liuqi0 <809270376@qq.com>
2015-07-16 15:08:28 +0800

    fulltext keywords

--- client/views/publications/articles/sections/fulltext.js
+++ client/views/publications/articles/sections/fulltext.js
@@ -103,3 +103,33 @@ Template.tableModal.helpers({
 		return Session.get("table").table;
 	}
 });
+
+Template.FullTextTemplate.helpers({
+    getKeywords:function(){
+        var result=[];
+        var total=0;
+        _.each(this.keywords,function(item){
+            var keyword={};
+            keyword.word=item;
+            keyword.score=Keywords.findOne({name:item}).score;
+            total+=keyword.score;
+            result.push(keyword);
+        });
+        _.each(result,function(item){
+            item.percent = item.score / total * 100;
+        });
+        return  _.sortBy(result,function(obj){
+            return -obj.score;
+        });
+    },
+    getScoreByKeyword: function (k) {
+        return Keywords.findOne({name: k}).score;
+    },
+    totals: function (a) {
+        var s = 0;
+        a.forEach(function(k){
+            s += Keywords.findOne({name: k}).score;
+        })
+        return s;
+    }
+});
