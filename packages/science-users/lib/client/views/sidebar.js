079f01afba8350f77d8ac991a8a24acd89fdcbd0
arthuryyx <arthuryyx@hotmail.com>
2015-07-22 11:38:44 +0800

    display client ip on sidebar

--- packages/science-users/lib/client/views/sidebar.js
+++ packages/science-users/lib/client/views/sidebar.js
@@ -1,4 +1,19 @@
 Template.LayoutSideBar.helpers({
+	orgLogo: function () {
+		var currentUserIPNumber = Session.get("currentUserIPNumber");
+		if(currentUserIPNumber === undefined){
+			currentUserIPNumber = 0;
+			Meteor.call("getClientIP", function (err, ip) {
+				//var arr = ip.split('.');
+				//arr.reverse().forEach(function (a, index) {
+				//	currentUserIPNumber += (a*(Math.pow(256,index)));
+				//})
+				currentUserIPNumber = ip;
+				Session.set("currentUserIPNumber", currentUserIPNumber);
+			});
+		}
+		return currentUserIPNumber;
+	},
 	canUseAdminPanel:function(){
 		return !!Permissions.getUserRoles().length;
 	}
