a5e6588f3570fc6abfae8caa33007bdd812950d9
liuqi0 <809270376@qq.com>
2015-07-22 18:21:41 +0800

    report begin

---
+++ packages/science-reports/server/collection.js
@@ -0,0 +1,12 @@
+//ArticleCollections.before.insert(function (userId, doc) {
+//	doc.createdAt = new Date();
+//	doc.createdBy = userId;
+//	if (!doc.createdBy) doc.createdBy = userId;
+//});
+//
+//
+//ArticleCollections.before.update(function (userId, doc, fieldNames, modifier, options) {
+//	modifier.$set = modifier.$set || {};
+//	modifier.$set.modifiedAt = new Date();
+//	modifier.$set.modifiedBy = userId;
+//});
