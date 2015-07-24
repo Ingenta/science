a696239f1baa8381eb206c6e216d51f570aa1c93
xiancui <451763649@qq.com>
2015-07-24 09:33:17 +0800

    ad 广告

--- both/collections/advertisement.js
+++ both/collections/advertisement.js
@@ -1,5 +1,17 @@
 this.Advertisement = new Meteor.Collection("advertisement");
 
+this.Advertisement.allow({
+    insert: function (userId, doc) {
+        return Permissions.userCan("add-article", "resource", userId);
+    },
+    update: function (userId, doc) {
+        return Permissions.userCan("modify-article", "resource", userId);
+    },
+    remove: function (userId, doc) {
+        return Permissions.userCan("delete-article", "resource", userId);
+    }
+});
+
 AdvertisementSchema = new SimpleSchema({
     title: {
         type: String,
