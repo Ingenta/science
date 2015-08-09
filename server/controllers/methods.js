5679873f50b55c462960c19e278886c69ce45996
jackkav <jackkav@gmail.com>
2015-08-09 19:09:42 +0800

    renamed a file, made rating stars smaller and set cursor pointers on publications page

---
+++ server/controllers/methods.js
@@ -0,0 +1,46 @@
+Meteor.methods({
+    'distinctVolume': function (journalId) {
+        result = Issues.distinct("volume", {"journalId": journalId});
+        console.dir(result);
+        return result;
+    },
+    'grabSessions': function (id) {
+        var c = UserStatus.connections.findOne({userId: id});
+        return c.ipAddr;
+    },
+    'getClientIP': function () {
+        return this.connection.httpHeaders['x-forwarded-for'] || this.connection.clientAddress;
+    },
+    'getMostRead': function () {
+        var a = ArticleViews.aggregate([{
+            $group: {
+                _id: {articleId: '$articleId'},
+                count: {$sum: 1}
+            }
+        }, {$sort: {count: -1}}]);
+        if (!a)return;
+        return a;
+    },
+    'countSession': function () {
+        var c = UserStatus.connections.find().count();
+        return c;
+    },
+    'sendEmail': function (to, from, subject, text) {
+        //check([to, from, subject, text], [String]);
+        if(!to || !from || !subject || !text){
+            console.log("Email not sent missing parameter.")
+            return;
+        }
+        // Let other method calls from the same client start running,
+        // without waiting for the email sending to complete.
+        this.unblock();
+
+        Email.send({
+            to: to,
+            from: from,
+            subject: subject,
+            text: text
+        });
+    }
+});
+
