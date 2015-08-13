5563db2a46331c9e069b4ab2a97e6ab3cf58a269
liuqi0 <809270376@qq.com>
2015-08-13 17:05:48 +0800

    xiao gai dong

--- packages/science-search/client/view/search.js
+++ packages/science-search/client/view/search.js
@@ -2,28 +2,28 @@ var pageSession = new ReactiveDict();
 
 
 Meteor.startup(function(){
-    Tracker.autorun(function(){
-        var query = pageSession.get("query");
-        Meteor.call("search",query,function(err,result){
-            var ok = err?false:result.responseHeader.status==0;
-            pageSession.set("ok",ok);
-            if(ok){
-                //pageSession.set("qtime",err?undefined:result.responseHeader.QTime);
-                if(result.response){
-                    pageSession.set("numFound",result.response.numFound);
-                    pageSession.set("start",result.response.start);
-                    pageSession.set("docs",undefined);
-                    pageSession.set("docs",result.response.docs);
-                }
-                if(result.facet_counts){
-                    pageSession.set("facets",result.facet_counts.facet_fields);
-                }
-                if(result.highlighting){
-                    pageSession.set("highlight",result.highlighting);
-                }
-            }
-        })
-    });
+//    Tracker.autorun(function(){
+//        var query = pageSession.get("query");
+//        Meteor.call("search",query,function(err,result){
+//            var ok = err?false:result.responseHeader.status==0;
+//            pageSession.set("ok",ok);
+//            if(ok){
+//                //pageSession.set("qtime",err?undefined:result.responseHeader.QTime);
+//                if(result.response){
+//                    pageSession.set("numFound",result.response.numFound);
+//                    pageSession.set("start",result.response.start);
+//                    pageSession.set("docs",undefined);
+//                    pageSession.set("docs",result.response.docs);
+//                }
+//                if(result.facet_counts){
+//                    pageSession.set("facets",result.facet_counts.facet_fields);
+//                }
+//                if(result.highlighting){
+//                    pageSession.set("highlight",result.highlighting);
+//                }
+//            }
+//        })
+//    });
 })
 
 Template.SolrSearchBar.events({
