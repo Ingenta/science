8a12e9c6db2541aa8fcae5e4f2dbb0ab4237b34d
arthuryyx <arthuryyx@hotmail.com>
2015-08-24 17:43:41 +0800

    institution account tab

--- packages/science-institution/client/view/admin/institutions/detail.js
+++ packages/science-institution/client/view/admin/institutions/detail.js
@@ -16,20 +16,26 @@ Template.institutionOptions.helpers({
 
 Template.institutionOptions.helpers({
     info: function () {
-		var obj = Institutions.findOne({_id: Router.current().params.insId});
-		return obj;
-	}
+        var obj = Institutions.findOne({_id: Router.current().params.insId});
+        return obj;
+    }
 });
 
 Template.institutionDetail.helpers({
-    getType: function(type){
-        switch (type){
-            case "1": return TAPi18n.__("Librarian");
-            case "2": return TAPi18n.__("Publisher");
-            case "3": return TAPi18n.__("Editorial Board");
-            case "4": return TAPi18n.__("University");
-            case "5": return TAPi18n.__("Research Institution");
-            case "6": return TAPi18n.__("Others");
+    getType: function (type) {
+        switch (type) {
+            case "1":
+                return TAPi18n.__("Librarian");
+            case "2":
+                return TAPi18n.__("Publisher");
+            case "3":
+                return TAPi18n.__("Editorial Board");
+            case "4":
+                return TAPi18n.__("University");
+            case "5":
+                return TAPi18n.__("Research Institution");
+            case "6":
+                return TAPi18n.__("Others");
         }
     }
 })
