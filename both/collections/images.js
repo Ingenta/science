0f1386901d9138f1861b41e120449da01d3fc57d
蒋凯 <kai.jiang@digitalpublishing.cn>
2015-08-25 11:56:13 +0800

    allow update image

--- both/collections/images.js
+++ both/collections/images.js
@@ -5,7 +5,12 @@ Images.allow({
     insert: function (userId, doc) {
         return true;
     },
+    update:function(userId,doc){
+        return true;
+    },
     download: function (userId) {
         return true;
     }
 });
+
+//TODO: permission check
