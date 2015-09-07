48b559fce63e8c2691ac44c731053a6674611315
蒋凯 <kai.jiang@digitalpublishing.cn>
2015-09-07 21:27:47 +0800

    disable test code

--- packages/science-auto-task/server/cron.js
+++ packages/science-auto-task/server/cron.js
@@ -1,15 +1,15 @@
-SyncedCron.add({
-	name:'test my cron',
-	schedule: function(parser){
-		return parser.text('every 2 sec');
-	},
-	job: function(){
-		var numbersCrunched = 121212;
-		console.log(numbersCrunched);
-		return numbersCrunched;
-	}
-});
-
-Meteor.startup(function(){
-	SyncedCron.start();
-})
+//SyncedCron.add({
+//	name:'DOI Register(注册DOI)',
+//	schedule: function(parser){
+//		return parser.text('every 10 sec');
+//	},
+//	job: function(){
+//		var numbersCrunched = 121212;
+//		console.log(numbersCrunched);
+//		return numbersCrunched;
+//	}
+//});
+//
+//Meteor.startup(function(){
+//	SyncedCron.start();
+//})
