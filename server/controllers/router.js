c099248b5940afc392c0d7ec148034c889c41b05
arthuryyx <arthuryyx@hotmail.com>
2015-09-16 18:12:28 +0800

    citation export plain text

--- server/controllers/router.js
+++ server/controllers/router.js
@@ -1,3 +1,33 @@
 Router.map(function () {
+    this.route('txtFile', {
+        where: 'server',
+        path: '/citation/plaintext/:publisherDoi/:articleDoi',
+        action: function () {
+            var obj = Articles.findOne({doi: this.params.publisherDoi + "/" + this.params.articleDoi});
+            var text = undefined;
+            if (obj.language == 1) {
+                text = obj.title.en;
+                obj.authors.forEach(function (author) {
+                    text += author.given.en + ", " + author.surname.en + ", ";
+                });
+                text += obj.journal.title + ", ";
+            } else {
+                text = obj.title.cn;
+                obj.authors.forEach(function (author) {
+                    text += author.given.cn + ", " + author.surname.cn + ", ";
+                });
+                text += obj.journal.titleCn + ", ";
+            }
+            text += obj.volume + ", " + obj.elocationId + " (" + obj.year + "), DOI:http://dx.doi.org/" + this.params.publisherDoi + "/" + this.params.articleDoi;
 
+            var filename = this.params.articleDoi + '.txt';
+            var headers = {
+                'Content-Type': 'text/plain',
+                'Content-Disposition': "attachment; filename=" + filename
+            };
+
+            this.response.writeHead(200, headers);
+            return this.response.end(text);
+        }
+    })
 });
