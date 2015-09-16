ec8b10b7821e92349d41656d58d50d9ca0f971bd
蒋凯 <kai.jiang@digitalpublishing.cn>
2015-09-17 00:30:07 +0800

    兴趣检索

--- client/views/publications/articles/authorPopButton.js
+++ client/views/publications/articles/authorPopButton.js
@@ -4,13 +4,6 @@ Template.authorPopButton.events({
         var ele = $(event.currentTarget);
         var name = Template.currentData().name;
         var clearname=Science.clearTags(name);
-        //var html = "<div class='author-pop'>";
-        //html += "<div><a href='/author/" + clearname + "'>" + TAPi18n.__("Go to author page") + "</a></div>";
-        //html += "<hr>";
-        //html += "<div><a target='_blank' href='http://www.ncbi.nlm.nih.gov/pubmed?term=" + clearname + "[Author]'>PubMed</a></div>";
-        //html += "<hr>";
-        //html += "<div><a target='_blank' href='http://scholar.google.com/scholar?q=author:" + clearname + "'>Google Scholar</a></div>";
-        //html += "</div>";
         var title = Blaze.toHTMLWithData(Template.authorPopTitle,{
             name:name
         });
--- packages/science-search/client/query.js
+++ packages/science-search/client/query.js
@@ -80,6 +80,48 @@ SolrQuery = {
 			qString += (l + "fq=" + fqStrArr.join("&fq="));
 		}
 		Router.go('/search' + qString);
+	},
+	interstingSearch:function(event){
+		var content = Science.dom.getSelContent();
+		if(content){
+			console.log(content);
+			var trimContent = content.trim();
+			if(trimContent.length >2 && trimContent.length < 100){
+
+				var journalId = Session.get('currentJournalId');
+				var filterQuery = [{journalId:journalId}];
+				var setting = {rows:5};
+				Meteor.call("search",trimContent,filterQuery,setting,function(err,result){
+					var ok = err?false:result.responseHeader.status==0;
+					if(ok){
+						console.log(result);
+						var htmlContent = Blaze.toHTMLWithData(Template.selectionSearch, {
+							result:result
+						});
+
+						var pointEle = $(".point-ele");
+						if(!pointEle || !pointEle.length){
+							pointEle=$('<a href="#" class="point-ele" role="button" tabindex="0" data-toggle="popover" ></a>');
+							$("body").append(pointEle);
+						}
+						pointEle.on('hidden.bs.popover',function(){
+							pointEle.popover('destroy');
+							pointEle.remove();
+						});
+						pointEle.css({left: event.pageX,top: event.pageY});
+						pointEle.popover({
+							html:true,
+							content:htmlContent,
+							title:"兴趣检索",
+							trigger:"focus"
+						});
+						pointEle.focus();
+					}
+				})
+
+
+			}
+		}
 	}
 };
 
