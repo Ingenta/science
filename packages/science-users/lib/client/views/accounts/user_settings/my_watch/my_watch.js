05d07e573a7e442196d848f8d1164d0ee7def120
liuqi0 <809270376@qq.com>
2015-09-17 16:51:51 +0800

    delete

--- packages/science-users/lib/client/views/accounts/user_settings/my_watch/my_watch.js
+++ packages/science-users/lib/client/views/accounts/user_settings/my_watch/my_watch.js
@@ -2,26 +2,6 @@ ReactiveTabs.createInterface({
     template: 'watchTabs'
 });
 
-//Template.UserSettingsMyWatch.helpers({
-//    watch : function(){
-//        var user = Users.findOne({_id: Meteor.userId()});
-//        console.log(user.watch.length);
-//        for(var i=0;i<user.watch.length;i++) {
-//            console.log(i);
-//            return Publications.find({"_id": user.watch[i]});
-//        }
-//    }
-//})
-
-//Template.SingleWatch.helpers({
-//    JournalUrl: function (Jourid) {
-//        var publication = Publications.findOne({_id:Jourid});
-//        var publisher = Publishers.findOne({_id: publication.publisher});
-//        var urls = "/publisher/"+publisher.name+"/journal/"+publication.title;
-//        return urls;
-//    }
-//})
-
 Template.watchOptions.helpers({
     tabs: function () {
         return [
