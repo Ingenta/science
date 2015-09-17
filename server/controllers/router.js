442d90bde9dff350c2442821d157afe61e4490bf
arthuryyx <arthuryyx@hotmail.com>
2015-09-17 14:22:11 +0800

    endnote

--- server/controllers/router.js
+++ server/controllers/router.js
@@ -62,5 +62,41 @@ Router.map(function () {
             this.response.writeHead(200, headers);
             return this.response.end(text);
         }
-    })
+    });
+    this.route('Endnote', {
+        where: 'server',
+        path: '/citation/endnote/:publisherDoi/:articleDoi',
+        action: function () {
+            var obj = Articles.findOne({doi: this.params.publisherDoi + "/" + this.params.articleDoi});
+            var publisher = Publishers.findOne({_id: obj.publisher});
+            var name1 = "/publisher/" + publisher.name + "/journal/" + obj.journal.title + "/" + obj.volume + "/" + obj.issue + "/" + obj.doi;
+            var text = "%0 Journal Article\n";
+            if (obj.language == 1) {
+                obj.authors.forEach(function (author) {
+                    text += "%A " + (author.given.en || author.given.cn) + ", " + (author.surname.en || author.surname.cn) + "\n";
+                });
+                text += "%T " + (obj.title.en || obj.title.cn) + "\n%D " + obj.year + "\n%J " + (obj.journal.title || obj.journal.titleCn);
+            } else {
+                obj.authors.forEach(function (author) {
+                    text += "%A " + (author.given.cn || author.given.en) + ", " + (author.surname.cn || author.surname.en) + "\n";
+                });
+                text += "%T " + (obj.title.cn || obj.title.en) + "\n%D " + obj.year + "\n%J " + (obj.journal.titleCn || obj.journal.title);
+            }
+            text += "\n%V " + obj.volume + "\n%N " + obj.issue + "\n%P " + obj.elocationId + "\n%R doi:http://dx.doi.org/" + obj.doi + "\n";
+
+            obj.keywords.forEach(function (keyword) {
+                text += "%K " + keyword + "\n";
+            });
+            text +="%U " + Config.host.url + name1 + "\n";
+
+            var filename = this.params.articleDoi + '.enw';
+            var headers = {
+                'Content-Type': 'text/plain',
+                'Content-Disposition': "attachment; filename=" + filename
+            };
+
+            this.response.writeHead(200, headers);
+            return this.response.end(text);
+        }
+    });
 });
