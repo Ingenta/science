272947292fa56a14227836c137ebbf27ddacbd91
jackkav <jackkav@gmail.com>
2015-09-18 10:32:11 +0800

    email tweak

--- client/views/helper.js
+++ client/views/helper.js
@@ -1,25 +1,33 @@
 Template.toggleField.helpers({
-    getContent: function(field){
-        if (typeof field == "object"){
+    getContent: function (field) {
+        if (typeof field == "object") {
             if (TAPi18n.getLanguage() === "zh-CN") {
                 if (!field.cn)return field.en;
                 return field.cn;
             }
             if (!field.en)return field.cn;
             return field.en;
-        }else{
+        } else {
             return field;
         }
     }
 })
 Template.sendEmails.events({
     "click .btn-primary": function () {
-        console.log("abc")
+        if (!Meteor.user())return;
+        if (!Meteor.user().emails[0])return;
+        if (!Meteor.user().emails[0].address)return;
+
+        var user = Meteor.user().emails[0].address;
+        if (Meteor.user().profile)
+            if (Meteor.user().profile.realname)
+                user = Meteor.user().profile.realname;
         Meteor.call('sendEmail',
-            'jack.kavanagh@digitalpublishing.cn',
+            Meteor.user().emails[0].address,
             'eryaer@sina.com',
-            'Hello from Meteor!',
-            'Here is an article you might be interested in. \n\n' + Router.current().url);
+            user + ' has sent you an article',
+            'Click the link below to check it out. \n\n' + Router.current().url);
+
     }
 })
 
