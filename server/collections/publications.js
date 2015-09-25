e4ad40032b12c973276e0b9d8dbb817bffcc1612
arthuryyx <arthuryyx@hotmail.com>
2015-09-25 13:03:18 +0800

    fix historical journal can be null

--- server/collections/publications.js
+++ server/collections/publications.js
@@ -2,10 +2,12 @@ Publications.before.insert(function (userId, doc) {
     doc.createdAt = new Date();
     doc.createdBy = userId;
     if (!doc.createdBy) doc.createdBy = userId;
+    if (doc.historicalJournal) doc.historicalJournal = _.compact(doc.historicalJournal);
 });
 
 Publications.before.update(function (userId, doc, fieldNames, modifier, options) {
     modifier.$set = modifier.$set || {};
     modifier.$set.modifiedAt = new Date();
     modifier.$set.modifiedBy = userId;
+    if (modifier.$set.historicalJournal) modifier.$set.historicalJournal = _.compact(modifier.$set.historicalJournal);
 });
