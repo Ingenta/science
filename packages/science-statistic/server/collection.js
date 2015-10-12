c1bfdc8701d0a30494b03b250eae4d2b9f86fa51
蒋凯 <kai.jiang@digitalpublishing.cn>
2015-10-12 17:37:24 +0800

    little fix

--- packages/science-statistic/server/collection.js
+++ packages/science-statistic/server/collection.js
@@ -1,7 +1,7 @@
 Logs = new Meteor.Collection("Logs");
 
 Meteor.startup(function(){
-	if(Logs.count()==0){
+	if(Logs.find().count()==0){
 		var titles = ["linux私房菜","Java编程思想","数学之美","浪潮之巅","算法","疯狂Android讲义"];
 		var users =  ["jack","rose","rick","lily","lucy","ben"];
 		var actives = ["read","recommend","fav","download"];
@@ -12,7 +12,7 @@ Meteor.startup(function(){
 				year:2000+Math.getRandom()%14,
 				active:actives[Math.getRandom()%4],
 				month:Math.getRandom()%12+1
-			}); 
+			});
 		}
 	}
 })
