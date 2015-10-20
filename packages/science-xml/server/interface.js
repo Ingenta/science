ed9c673b3d6062c343bb4310b353b7245fb7a37d
蒋凯 <kai.jiang@digitalpublishing.cn>
2015-10-20 18:51:28 +0800

    ftp接口调整，可提示更多错误。

--- packages/science-xml/server/interface.js
+++ packages/science-xml/server/interface.js
@@ -1,39 +1,52 @@
 Router.route('/api', function () {
 	var req            = this.request;
 	var res            = this.response;
+	var result = {};
+	res.writeHead(200,{
+		'Content-Type': 'applications/json'
+	});
+	if(req.method !=='POST'){
+		result.result="failed";
+		result.message="wrong method, please use POST method with 'x-www-form-urlencoded'";
+		res.write(JSON.stringify(result));
+		res.end();
+		return;
+	}
 	var NonEmptyString = Match.Where(function (x) {
 		check(x, String);
 		return x.length > 0;
 	});
-	check(req.body, {
-		user      : NonEmptyString,
-		password  : NonEmptyString,
-		host      : NonEmptyString,
-		sourcePath: NonEmptyString
-	});
+	try{
+		check(req.body, {
+			user      : NonEmptyString,
+			password  : NonEmptyString,
+			host      : NonEmptyString,
+			sourcePath: NonEmptyString
+		});
+	}catch(e){
+		result.result="failed";
+		result.message="miss params";
+		res.write(JSON.stringify(result));
+		res.end();
+	}
+
 	var slashLoc       = req.body.sourcePath.lastIndexOf("/")+1;
 	var filename       = slashLoc == 0 ? req.body.sourcePath : req.body.sourcePath.substr(slashLoc);
 	var targetPath     = Config.ftp.downloadDir + "/" + filename;
 	//console.log("download:"+targetPath);
 	ScienceXML.FolderExists(Config.ftp.downloadDir);
 	var downloadCallback = function(err){
-		res.writeHead(200,{
-			'Content-Type': 'applications/json'
-		});
-		var result = {};
 		if(err){
 			result.result="failed";
 			result.message=err.message;
 		}else{
 			result.result="success";
+			Tasks.startJob(targetPath,filename,"application/zip");
 		}
 		res.write(JSON.stringify(result));
 
-
-		Tasks.startJob(targetPath,filename,"application/zip");
 		res.end();
 	};
-
 	(new FTP()).getSingleFile(req.body, targetPath, Meteor.bindEnvironment(downloadCallback));
 
 }, {where: 'server'});
