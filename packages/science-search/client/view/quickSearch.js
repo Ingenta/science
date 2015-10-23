8753de10a0ee49f37facb1d2fa0f368d3dab85ea
Jack Kavanagh <jackkav@gmail.com>
2015-10-23 13:58:03 +0800

    added some null checking

--- packages/science-search/client/view/quickSearch.js
+++ packages/science-search/client/view/quickSearch.js
@@ -1,38 +1,39 @@
 Template.quickSearchTemplate.helpers({
-	journals:function(){
-		return Publications.find({},{field:{title:1,titleCn:1}});
-	},
-	isJournalPage:function(){
-		var currRoute = Router.current().route.getName();
-		return _.contains(['journal.name','journal.name.volume','article.show','guidelines.show'],currRoute);
-	},
-	journal:function(){
-		var jourId = Session.get('currentJournalId');
-		var journal = Publications.findOne({_id:jourId},{field:{title:1,titleCn:1}});
-		return journal;
-	}
+    journals: function () {
+        return Publications.find({}, {field: {title: 1, titleCn: 1}});
+    },
+    isJournalPage: function () {
+        if (!Router.current().route)return false;
+        var currRoute = Router.current().route.getName();
+        return _.contains(['journal.name', 'journal.name.volume', 'article.show', 'guidelines.show'], currRoute);
+    },
+    journal: function () {
+        var jourId = Session.get('currentJournalId');
+        var journal = Publications.findOne({_id: jourId}, {field: {title: 1, titleCn: 1}});
+        return journal;
+    }
 });
 
 Template.quickSearchTemplate.events({
-	'click .quick-search':function(e){
-		e.stopPropagation();
-	},
-	'click .qs-submit':function(e){
-		e.preventDefault();
-		var journalId = Template.instance().$('.qs-journal').val() || Template.instance().$('.qs-journal-hidden').val();
-		var volume = Template.instance().$('.qs-volume').val();
-		var issue = Template.instance().$('.qs-issue').val();
-		var page = Template.instance().$('.qs-page').val();
-		var query = {filterQuery:{}};
-		if(journalId)
-			query.filterQuery.journalId=[journalId];
-		if(volume)
-			query.filterQuery.volume=[volume];
-		if(issue)
-			query.filterQuery.issue=[issue];
-		if(page)
-			query.filterQuery.page=[page];
-		Template.instance().$('.dropdown-toggle').dropdown('toggle');
-		SolrQuery.search(query);
-	}
+    'click .quick-search': function (e) {
+        e.stopPropagation();
+    },
+    'click .qs-submit': function (e) {
+        e.preventDefault();
+        var journalId = Template.instance().$('.qs-journal').val() || Template.instance().$('.qs-journal-hidden').val();
+        var volume = Template.instance().$('.qs-volume').val();
+        var issue = Template.instance().$('.qs-issue').val();
+        var page = Template.instance().$('.qs-page').val();
+        var query = {filterQuery: {}};
+        if (journalId)
+            query.filterQuery.journalId = [journalId];
+        if (volume)
+            query.filterQuery.volume = [volume];
+        if (issue)
+            query.filterQuery.issue = [issue];
+        if (page)
+            query.filterQuery.page = [page];
+        Template.instance().$('.dropdown-toggle').dropdown('toggle');
+        SolrQuery.search(query);
+    }
 });
