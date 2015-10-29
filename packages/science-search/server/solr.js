2142805061b261f7bc323b8ee5b7ce6b24734b0c
Jack Kavanagh <jackkav@gmail.com>
2015-10-29 11:00:55 +0800

    changed solr config so it doesn't lose connection from meteor -> solr

--- packages/science-search/server/solr.js
+++ packages/science-search/server/solr.js
@@ -1,6 +1,6 @@
 SolrClient = Solr.createClient({
-	host: "http://" + (process.env.SOLR_PORT_8983_TCP_ADDR || "192.168.99.100"),
-	port:"8983",
-	core:"/articles",
-	path:"/solr"
+    host: "http://" + (Meteor.isDevelopment ? "192.168.99.100" : "solr"),
+    port: "8983",
+    core: "/articles",
+    path: "/solr"
 });
