8b14fd519c3c27ab57dfcbaa992a11c84a3f1005
arthuryyx <arthuryyx@hotmail.com>
2015-10-29 13:13:37 +0800

    reconstruct permissions

--- packages/science-permissions/group/advertisement.js
+++ packages/science-permissions/group/advertisement.js
@@ -1,21 +1,34 @@
 advertisementPermissions = new Permissions.Registrar('advertisement');
 
 advertisementPermissions
-    .definePermission("add-advertisement", {
-        en: {name: "add advertisement", summary: "can add advertisement"},
-        cn: {name: "添加广告", summary: "您可以添加新广告到系统中"}
+    .definePermission("add-global-advertisement", {
+        en: {name: "add global　advertisement", summary: "can add global　advertisement"},
+        cn: {name: "添加平台广告", summary: "您可以添加新平台广告到系统中"}
     })
-    .definePermission("modify-advertisement", {
-        en: {name: "modify advertisement", summary: "can modify advertisement"},
-        cn: {name: "编辑广告", summary: "可以编辑广告信息"}
+    .definePermission("modify-global-advertisement", {
+        en: {name: "modify　global advertisement", summary: "can modify global　advertisement"},
+        cn: {name: "编辑平台广告", summary: "可以编辑平台广告信息"}
     })
-    .definePermission("delete-advertisement", {
-        en: {name: "delete advertisement", summary: "can delete advertisement"},
-        cn: {name: "删除广告", summary: "您可以删除广告信息"}
+    .definePermission("delete-global-advertisement", {
+        en: {name: "delete global　advertisement", summary: "can delete global　advertisement"},
+        cn: {name: "删除平台广告", summary: "您可以删除平台广告信息"}
+    })
+
+    .definePermission("add-journal-advertisement", {
+        en: {name: "add journal　advertisement", summary: "can add journal　advertisement"},
+        cn: {name: "添加期刊主页广告", summary: "您可以添加新期刊主页广告到系统中"}
+    })
+    .definePermission("modify-journal-advertisement", {
+        en: {name: "modify　journal advertisement", summary: "can modify journal　advertisement"},
+        cn: {name: "编辑期刊主页广告", summary: "可以编辑期刊主页广告信息"}
+    })
+    .definePermission("delete-journal-advertisement", {
+        en: {name: "delete journal　advertisement", summary: "can delete journal　advertisement"},
+        cn: {name: "删除期刊主页广告", summary: "您可以删除期刊主页广告信息"}
     });
 
 advertisementPermissions
-    .defineRole("advertisement-manager", ["add-advertisement", "modify-advertisement", "delete-advertisement"], {
+    .defineRole("advertisement-manager-publisher", ["add-journal-advertisement", "modify-journal-advertisement", "delete-journal-advertisement"], {
         en: {
             name: "advertisement manager (publisher)",
             summary: "advertisement manager"
