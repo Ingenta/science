92e75843f050798254f78b223b31f9a798d1d90e
Jack Kavanagh <jackkav@gmail.com>
2015-10-29 13:30:23 +0800

    bug fix in watch and refactor

--- packages/science-users/lib/client/views/accounts/user_settings/my_watch/my_topicWatch.js
+++ packages/science-users/lib/client/views/accounts/user_settings/my_watch/my_topicWatch.js
@@ -1,21 +1,22 @@
 Template.TopicWatch.helpers({
-    watch : function(){
+    watch: function () {
         var user = Users.findOne({_id: Meteor.userId()});
-        return user.profile.interestedOfTopics;
+        if (user && user.profile)
+            return user.profile.interestedOfTopics;
     },
-    topicWatch: function(){
+    topicWatch: function () {
         return Topics.findOne({_id: this.toString()});
     },
-    count : function () {
-        if(Users.findOne().profile)
-        if(Users.findOne().profile.interestedOfTopics)
-        return Users.findOne().profile.interestedOfTopics.length;
+    count: function () {
+        if (Users.findOne().profile)
+            if (Users.findOne().profile.interestedOfTopics)
+                return Users.findOne().profile.interestedOfTopics.length;
     }
 })
 Template.TopicWatch.events({
     'click .btn': function () {
-        var selected =  _.pluck($("input[name='selectedTopic']:checked"),'value');
-        var diff = _.difference(Meteor.user().profile.interestedOfTopics,selected);
-        Users.update({_id:Meteor.userId()},{$set:{"profile.interestedOfTopics": diff}});
+        var selected = _.pluck($("input[name='selectedTopic']:checked"), 'value');
+        var diff = _.difference(Meteor.user().profile.interestedOfTopics, selected);
+        Users.update({_id: Meteor.userId()}, {$set: {"profile.interestedOfTopics": diff}});
     }
 })
