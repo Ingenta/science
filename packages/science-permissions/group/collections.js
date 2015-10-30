bad7332e3e35539f2277201eca6fd7d2cf090b88
arthuryyx <arthuryyx@hotmail.com>
2015-10-30 18:52:08 +0800

    redefine collection permissions

--- packages/science-permissions/group/collections.js
+++ packages/science-permissions/group/collections.js
@@ -1,70 +1,75 @@
 collPermissions = new Permissions.Registrar('collections');
 
 collPermissions
-	//平台文章集
-	.definePermission("add-big-collections", {
-		en: {name: "create big collections", summary: "can create big collection "},
-		cn: {name: "创建大文章集", summary: "可以创建文章选自同一出版社的大文章集"}
-	})
-	.definePermission("modify-big-collections", {
-		en: {name: "Modify big collections", summary: "can Modify big collection "},
-		cn: {name: "修改大文章集", summary: "可以修改文章选自同一出版社的大文章集"}
-	})
-	.definePermission("del-big-collections", {
-		en: {name: "delete big collections", summary: "can Delete big collection "},
-		cn: {name: "删除大文章集", summary: "可以删除文章选自同一出版社的大文章集"}
-	})
-	.definePermission("add-article-big-collections", {
-		en: {name: "add article to big collections", summary: "can Add article to big collection "},
-		cn: {name: "添加文章到大文章集", summary: "添加文章到大文章集"}
-	})
-	.definePermission("remove-article-big-collections", {
-		en: {name: "remove article from big collections", summary: "can Remove article from big collection "},
-		cn: {name: "从大文章集移除文章", summary: "从大文章集移除文章"}
-	})
-	//跨刊文章集
-	.definePermission("add-publisher-collections", {
-		en: {name: "create publisher collections", summary: "can create cross journal collection "},
-		cn: {name: "创建跨刊文章集", summary: "可以创建文章选自同一出版社的文章集"}
-	})
-	.definePermission("modify-publisher-collections", {
-		en: {name: "Modify publisher collections", summary: "can Modify cross journal collection "},
-		cn: {name: "修改跨刊文章集", summary: "可以修改文章选自同一出版社的文章集"}
-	})
-	.definePermission("del-publisher-collections", {
-		en: {name: "delete publisher collections", summary: "can Delete cross journal collection "},
-		cn: {name: "删除跨刊文章集", summary: "可以删除文章选自同一出版社的文章集"}
-	})
-	.definePermission("add-article-publisher-collections", {
-		en: {name: "add article to publisher collections", summary: "can Add article to cross journal collection "},
-		cn: {name: "添加文章到跨刊文章集", summary: "添加文章到跨刊文章集"}
-	})
-	.definePermission("remove-article-publisher-collections", {
-		en: {name: "remove article from publisher collections", summary: "can Remove article from cross journal collection "},
-		cn: {name: "从跨刊文章集移除文章", summary: "从跨刊文章集移除文章"}
-	})
-	//期刊文章集
-	.definePermission("add-journal-collections", {
-		en: {name: "create big collections", summary: "can create single journal collection "},
-		cn: {name: "创建期刊文章集", summary: "可以创建文章选自同一期刊的文章集"}
-	})
-	.definePermission("modify-journal-collections", {
-		en: {name: "Modify big collections", summary: "can Modify single journal collection "},
-		cn: {name: "修改期刊文章集", summary: "可以修改文章选自同一期刊的文章集"}
-	})
-	.definePermission("del-journal-collections", {
-		en: {name: "delete big collections", summary: "can Delete single journal collection "},
-		cn: {name: "删除期刊文章集", summary: "可以删除文章选自同一期刊的文章集"}
-	})
-	.definePermission("add-article-journal-collections", {
-		en: {name: "add article to journal collections", summary: "can Add article to single journal collection "},
-		cn: {name: "添加文章到期刊文章集", summary: "添加文章到期刊文章集"}
-	})
-	.definePermission("remove-article-journal-collections", {
-		en: {name: "remove article from journal collections", summary: "can Remove article from single journal collection "},
-		cn: {name: "从期刊文章集移除文章", summary: "从期刊文章集移除文章"}
-	});
-
+    //跨刊文章集
+    .definePermission("add-publisher-collection", {
+        en: {name: "create publisher collection", summary: "can create cross journal collection "},
+        cn: {name: "创建跨刊文章集", summary: "可以创建文章选自同一出版社的文章集"}
+    })
+    .definePermission("modify-publisher-collection", {
+        en: {name: "Modify publisher collection", summary: "can Modify cross journal collection "},
+        cn: {name: "修改跨刊文章集", summary: "可以修改文章选自同一出版社的文章集"}
+    })
+    .definePermission("del-publisher-collection", {
+        en: {name: "delete publisher collection", summary: "can Delete cross journal collection "},
+        cn: {name: "删除跨刊文章集", summary: "可以删除文章选自同一出版社的文章集"}
+    })
+    .definePermission("add-article-to-publisher-collection", {
+        en: {name: "add article to publisher collection", summary: "can Add article to cross journal collection "},
+        cn: {name: "添加文章到跨刊文章集", summary: "添加文章到跨刊文章集"}
+    })
+    .definePermission("remove-article-from-publisher-collection", {
+        en: {
+            name: "remove article from publisher collection",
+            summary: "can Remove article from cross journal collection "
+        },
+        cn: {name: "从跨刊文章集移除文章", summary: "从跨刊文章集移除文章"}
+    })
+    //期刊文章集
+    .definePermission("add-journal-collection", {
+        en: {name: "create journal collection", summary: "can create single journal collection "},
+        cn: {name: "创建期刊文章集", summary: "可以创建文章选自同一期刊的文章集"}
+    })
+    .definePermission("modify-journal-collection", {
+        en: {name: "Modify journal collection", summary: "can Modify single journal collection "},
+        cn: {name: "修改期刊文章集", summary: "可以修改文章选自同一期刊的文章集"}
+    })
+    .definePermission("del-journal-collection", {
+        en: {name: "delete journal collection", summary: "can Delete single journal collection "},
+        cn: {name: "删除期刊文章集", summary: "可以删除文章选自同一期刊的文章集"}
+    })
+    .definePermission("add-article-to-journal-collection", {
+        en: {name: "add article to journal collection", summary: "can Add article to single journal collection "},
+        cn: {name: "添加文章到期刊文章集", summary: "添加文章到期刊文章集"}
+    })
+    .definePermission("remove-article-from-journal-collection", {
+        en: {
+            name: "remove article from journal collection",
+            summary: "can Remove article from single journal collection "
+        },
+        cn: {name: "从期刊文章集移除文章", summary: "从期刊文章集移除文章"}
+    })
+    //专题辑
+    .definePermission("add-special-issue", {
+        en: {name: "create special issue", summary: "can create special issue "},
+        cn: {name: "创建专题辑", summary: "可以创建文章选自同一期的专题辑"}
+    })
+    .definePermission("modify-issue", {
+        en: {name: "Modify special issue", summary: "can Modify special issue "},
+        cn: {name: "修改专题辑", summary: "可以修改文章选自同一期的专题辑"}
+    })
+    .definePermission("del-issue", {
+        en: {name: "delete special issue", summary: "can Delete special issue "},
+        cn: {name: "删除专题辑", summary: "可以删除文章选自同一期的专题辑"}
+    })
+    .definePermission("add-article-to-special-issue", {
+        en: {name: "add article to special issue", summary: "can Add article to special issue "},
+        cn: {name: "添加文章到专题辑", summary: "添加文章到的专题辑"}
+    })
+    .definePermission("remove-article-from-special-issue", {
+        en: {name: "remove article from special issue", summary: "can Remove article from special issue "},
+        cn: {name: "从专题辑移除文章", summary: "从专题辑移除文章"}
+    });
 //collPermissions
 //	.defineRole("collections-manager", ["add-publisher-collections", "modify-publisher-collections", "delete-publisher-collections"], {
 //		en: {
--- packages/science-permissions/group/publication.js
+++ packages/science-permissions/group/publication.js
@@ -10,7 +10,19 @@ resourcePermissions
 
         "advertisement:add-journal-advertisement",
         "advertisement:modify-journal-advertisement",
-        "advertisement:delete-journal-advertisement"
+        "advertisement:delete-journal-advertisement",
+
+        "collections:add-journal-collection",
+        "collections:modify-journal-collection",
+        "collections:delete-journal-collection",
+        "collections:add-article-to-journal-collection",
+        "collections:remove-article-from-journal-collection",
+
+        "collections:add-special-issue",
+        "collections:modify-special-issue",
+        "collections:delete-special-issue",
+        "collections:add-article-to-special-issue",
+        "collections:remove-article-from-special-issue"
     ], {
         en: {
             name: "journal manager (publisher)",
