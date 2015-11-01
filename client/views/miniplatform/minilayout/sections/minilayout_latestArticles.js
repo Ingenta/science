eae432925460067f54d57c5de720ee28fe330bd7
liuqi0 <809270376@qq.com>
2015-11-01 11:45:36 +0800

    mini latest article

--- client/views/miniplatform/minilayout/sections/minilayout_latestArticles.js
+++ client/views/miniplatform/minilayout/sections/minilayout_latestArticles.js
@@ -4,6 +4,15 @@ Template.layoutLatestArticles.helpers({
     }
 });
 
+Template.layoutLatestArticles.events({
+    'click #pubDel': function (e) {
+        var pid = this._id;
+        confirmDelete(e,function(){
+            NewsCenter.remove({_id:pid});
+        })
+    }
+})
+
 AutoForm.addHooks(['addNewsRecommendModalForm'], {
     onSuccess: function () {
         $("#addNewsRecommendModal").modal('hide');
