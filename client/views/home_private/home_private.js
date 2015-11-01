ae66ec5a4e3452a6a666c0ecba8ac095ec80c193
arthuryyx <arthuryyx@hotmail.com>
2015-11-01 18:10:44 +0800

    journal page latest upload article

--- client/views/home_private/home_private.js
+++ client/views/home_private/home_private.js
@@ -72,6 +72,8 @@ Template.SingleNews.events({
 
 Template.latestUploadedArticleList.helpers({
     latestUploadedArticles: function () {
+        if(this.journalId)
+            return Articles.find({journalId: this.journalId}, {sort: {createdAt: -1}, limit: 10});
         return Articles.find({}, {sort: {createdAt: -1}, limit: 10});
     }
 });
