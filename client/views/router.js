fdf362fbf518e7c695e3e22e0f26d0483a5dfb1c
arthuryyx <arthuryyx@hotmail.com>
2015-11-01 18:40:09 +0800

    most read more page add journal range

--- client/views/router.js
+++ client/views/router.js
@@ -155,6 +155,25 @@ Router.map(function () {
         }
     });
 
+    this.route('mostReadArticles/:journalId', {
+        template: "mostReadArticle",
+        title: function () {
+            return TAPi18n.__("Most read articles");
+        },
+        parent: "home",
+        name: "mostRead.showWithJournalId",
+        waitOn: function () {
+            return [
+                Meteor.subscribe('images'),
+                Meteor.subscribe('publishers'),
+                Meteor.subscribe('publications'),
+                Meteor.subscribe('articles'),
+                Meteor.subscribe('issues'),
+                Meteor.subscribe('files')
+            ]
+        }
+    });
+
     this.route('/mostCiteArticles', {
         template: "mostCiteArticle",
         title: function () {
