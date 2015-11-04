ba7cd81928332a1744a5943a483a24b155abaafb
Jack Kavanagh <jackkav@gmail.com>
2015-11-04 14:16:24 +0800

    fixed most read, set it not to reactively update to save page load speed

--- client/views/home_private/sections/most_read/most_read.js
+++ client/views/home_private/sections/most_read/most_read.js
@@ -1,22 +1,27 @@
 Template.mostReadArticleList.helpers({
     mostReadArticles: function () {
-        Meteor.call("getMostRead", Meteor.userId(), this.journalId, function (err, result) {
-            Session.set("mostRead", result);
-        });
+        if (!Session.get("mostRead")) {
+            Meteor.call("getMostRead", Meteor.userId(), this.journalId, function (err, result) {
+                Session.set("mostRead", result);
+            });
+        }
         var most = Session.get("mostRead");
         if (!most)return;
 
         //TODO: figure out a better way to do this instead of calling the db for each id in the list
         var mostReadArticles = [];
         var suggestedArticle = SuggestedArticles.findOne();
-        most.forEach(function (id) {
-            if (!suggestedArticle || id._id.articleId !== suggestedArticle.articleId) {
-                var article = Articles.findOne({_id: id._id.articleId});
+        if(suggestedArticle&&!this.journalId)
+        {
+            var article = Articles.findOne({_id: suggestedArticle.articleId});
+            article && mostReadArticles.push(article);
+        }
+        most.forEach(function (item) {
+            if (!suggestedArticle || item._id.articleId !== suggestedArticle.articleId) {
+                var article = Articles.findOne({_id: item._id.articleId});
                 article && mostReadArticles.push(article);
             }
         });
-        //get only the top 4 if there is a suggestion
-        if (suggestedArticle && !this.journalId)return _.first(mostReadArticles, [4]);
         return _.first(mostReadArticles, [5]);
     },
     mostReadCount: function () {
--- client/views/most/mostReadArticle.js
+++ client/views/most/mostReadArticle.js
@@ -1,29 +1,41 @@
 Template.mostReadArticle.events({
     'click .datesort': function (event) {
-        Session.set("sort",event.target.value);
+        Session.set("sort", event.target.value);
     }
 });
 
 Template.mostReadArticle.helpers({
     mostReadArticles: function () {
-        //Meteor.call("getMostRead", Meteor.userId(), function (err, result) {
-        //    Session.set("mostRead", result);
-        //});
-        // ArticleViews获取最多引用
+        if (!Session.get("mostRead")) {
+            Meteor.call("getMostRead", Meteor.userId(), this.journalId, function (err, result) {
+                Session.set("mostRead", result);
+            });
+        }
         var most = Session.get("mostRead");
         if (!most)return;
         // 获取更多Id
-        var allId=[];
-        _.each(most,function(item){
-            allId.push(item._id.articleId);
+        var allId = [];
+        var suggestedArticle = SuggestedArticles.findOne();
+        if (suggestedArticle) {
+            allId.push(suggestedArticle.articleId);
+        }
+        _.each(most, function (item) {
+            if(suggestedArticle&&!this.journalId) {
+                var article = Articles.findOne({_id: item._id.articleId});
+                article && allId.push(item._id.articleId);
+            }
         });
+        console.log(allId)
         // 返回article信息，并排序
-        var sort = {};
-        if(Session.get("sort"))
-            sort={"published":Session.get("sort")};
-        return Articles.find({_id:{$in:allId}},{sort:sort});
+        if (!Session.get("sort"))return _.map(allId, function (id) {
+            return Articles.findOne({_id: id})
+        })
+
+        var sort = {"published": Session.get("sort")};
+        return Articles.find({_id: {$in: allId}}, {sort: sort});
+
     },
-    query      : function () {
+    query: function () {
         return Router.current().params.searchQuery;
     }
 });
