203e086884f5d06b0e63fd08884beeffdf14257b
蒋凯 <kai.jiang@digitalpublishing.cn>
2015-11-04 12:44:55 +0800

    fixed some problem in past data parser

--- packages/science-parser/lib/parser.js
+++ packages/science-parser/lib/parser.js
@@ -16,7 +16,10 @@ Parser = function(filepath,options,callback){
 	 */
 	var getSimpleVal=function(xp,ele){
 		var val = xpath.select(xp,ele || dom);
-		return val && val.toString().trim();
+		if(_.isEmpty(val) || _.isEmpty(val[0].childNodes))
+			return;
+		var datas = _.pluck(val[0].childNodes,"data");
+		return datas.join("").trim();
 	};
 	/**
 	 * 同步方法
@@ -36,11 +39,24 @@ Parser = function(filepath,options,callback){
 	 */
 	var getMultiVal=function(xp,ele){
 		var obj = {};
+		var failedLang=[];
+		var successLang;
 		_.each(multiAttr,function(val,key){
 			obj[key]=getSimpleVal(xp.replace('{lang}',val),ele);
+			if(!obj[key])
+				failedLang.push(key);
+			else if(!successLang)
+				successLang=key;
 		});
+		//若某语种未取到值,检查此字段是否有成功取到值的字段,如果有则用该语种内容填补未取到值的语种.
+		if(!_.isEmpty(failedLang) && !_.isEmpty(multiAttr) && successLang){
+			_.each(failedLang,function(key){
+				obj[key]=obj[successLang];
+			})
+		}
 		return obj;
 	};
+
 	var getNodes=function(xp,ele){
 		return xpath.select(xp,ele || dom);
 	};
@@ -53,45 +69,45 @@ Parser = function(filepath,options,callback){
 	var parse = function(data){
 		dom = new Science.Dom().parseFromString(data);
 		var issue = {};
-		issue.issn=getSimpleVal("//issue/issn/text()");
-		issue.journalName = getSimpleVal("//issue/journalname/text()");
-		issue.title=getSimpleVal("//issue/title/text()");
-		issue.year=getSimpleVal("//issue/year/text()");
-		issue.volume=getSimpleVal("//issue/volume/text()");
-		issue.issue=getSimpleVal("//issue/number/text()");
+		issue.issn=getSimpleVal("//issue/issn");
+		issue.journalName = getSimpleVal("//issue/journalname");
+		issue.title=getSimpleVal("//issue/title");
+		issue.year=getSimpleVal("//issue/year");
+		issue.volume=getSimpleVal("//issue/volume");
+		issue.issue=getSimpleVal("//issue/number");
 		issue.articles=[];
 		var sections=getNodes("//issue/section");
 		_.each(sections,function(sec){
 			var articleNodes = getNodes("child::article",sec);
 			var property={};
-			property=getMultiVal("child::title[@locale='{lang}']/text()",sec);
+			property=getMultiVal("child::title[@locale='{lang}']",sec);
 			_.each(articleNodes,function(articleNode){
 				var article = {};
 				article.property=property;
-				article.language=getSimpleVal("child::language/text()",articleNode);
-				article.doi=getSimpleVal("child::id[@type=\"doi\"]/text()",articleNode);
-				article.title=getMultiVal("child::title[@locale='{lang}']/text()",articleNode);
-				article.subject=getMultiVal("child::subject[@locale='{lang}']/text()",articleNode);
-				article.subspecialty=getMultiVal("child::subspecialty[@locale='{lang}']/text()",articleNode);
-				article.abstract=getMultiVal("child::abstract[@locale='{lang}']/text()",articleNode);
-				article.indexing=getMultiVal("child::indexing/subject[@locale='{lang}']/text()",articleNode);
-				article.pages=getSimpleVal("child::pages/text()",articleNode);
-				article.startPage=getSimpleVal("child::start_page/text()",articleNode);
-				article.endPage=getSimpleVal("child::endPage/text()",articleNode);
+				article.language=getSimpleVal("child::language",articleNode);
+				article.doi=getSimpleVal("child::id[@type=\"doi\"]",articleNode);
+				article.title=getMultiVal("child::title[@locale='{lang}']",articleNode);
+				article.subject=getMultiVal("child::subject[@locale='{lang}']",articleNode);
+				article.subspecialty=getMultiVal("child::subspecialty[@locale='{lang}']",articleNode);
+				article.abstract=getMultiVal("child::abstract[@locale='{lang}']",articleNode);
+				article.indexing=getMultiVal("child::indexing/subject[@locale='{lang}']",articleNode);
+				article.pages=getSimpleVal("child::pages",articleNode);
+				article.startPage=getSimpleVal("child::start_page",articleNode);
+				article.endPage=getSimpleVal("child::endPage",articleNode);
 				article.pdf=getAttribute("child::galley/file/href/attribute::src",articleNode);
-				article.publishDate=getSimpleVal("child::publish_date/text()",articleNode);
-				article.acceptDate=getSimpleVal("child::accept_date/text()",articleNode);
+				article.publishDate=getSimpleVal("child::publish_date",articleNode);
+				article.acceptDate=getSimpleVal("child::accept_date",articleNode);
 				article.authors=[];
 				var authorNodes=getNodes("child::author",articleNode);
 				_.each(authorNodes,function(authorNode){
 					var author = {};
 					author.isPrimary=getAttribute("attribute::primary_contact",authorNode);
-					author.firstname=getSimpleVal("child::firstname/text()",authorNode);
-					author.middlename=getSimpleVal("child::middlename/text()",authorNode);
-					author.lastname=getSimpleVal("child::lastname/text()",authorNode);
-					author.authorname=getMultiVal("child::authorname[@locale='{lang}']/text()",authorNode);
-					author.affiliation=getMultiVal("child::affiliation[@locale='{lang}']/text()",authorNode);
-					author.corresp=getSimpleVal("child::email/text()",authorNode);
+					author.firstname=getSimpleVal("child::firstname",authorNode);
+					author.middlename=getSimpleVal("child::middlename",authorNode);
+					author.lastname=getSimpleVal("child::lastname",authorNode);
+					author.authorname=getMultiVal("child::authorname[@locale='{lang}']",authorNode);
+					author.affiliation=getMultiVal("child::affiliation[@locale='{lang}']",authorNode);
+					author.corresp=getSimpleVal("child::email",authorNode);
 					article.authors.push(author);
 				});
 				issue.articles.push(article);
