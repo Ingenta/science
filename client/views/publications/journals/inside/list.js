9fd5ebd2b5c2ef18a22c6e33fbca58f8ea82e1b4
liuqi0 <809270376@qq.com>
2015-11-05 18:34:59 +0800

    special topic

--- client/views/publications/journals/inside/list.js
+++ client/views/publications/journals/inside/list.js
@@ -1,8 +1,8 @@
 Template.articlesInSpecialTopics.helpers({
     articles: function () {
-        var currentTopic = Router.current().params.topicsId;
+        var currentTopic = Router.current().params.specialTopicsId;
         if (currentTopic)
-            return Articles.find({topic: currentTopic});
+            return Articles.find({specialTopics: currentTopic});
     }
 })
 
@@ -10,10 +10,10 @@ Template.singleArticleInSpecialTopics.events({
     "click button.btn-danger": function (e) {
         e.preventDefault();
         var that=this;
-        var topicsId = Router.current().params.topicsId;
+        var topicsId = Router.current().params.specialTopicsId;
         confirmDelete(e,function(){
-            var withOutThis = _.without(that.topic, topicsId);
-            Articles.update({_id: that._id}, {$set: {topic: withOutThis}});
+            var withOutThis = _.without(that.specialTopics, topicsId);
+            Articles.update({_id: that._id}, {$set: {specialTopics: withOutThis}});
         });
     }
 });
