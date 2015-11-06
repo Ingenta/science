e8d91a28ca33033f8b75b4f9bdcd1239e2697c95
arthuryyx <arthuryyx@hotmail.com>
2015-11-06 16:05:46 +0800

    fitle existing topic names form keyword search alert email

--- packages/science-emails/server/main.js
+++ packages/science-emails/server/main.js
@@ -1,14 +1,22 @@
 Science.Email = {};
 
 Science.Email.searchFrequencyEmail = function () {
-    var searchLogs = SearchLog.find({count: {$gte: Config.searchKeywordFrequency}}).fetch();
+    var topicNames = [];
+    Topics.find({}, {fields: {name: 1, englishName: 1}}).forEach(function (ondTopic) {
+        if(ondTopic.name) topicNames.push(ondTopic.name);
+        if(ondTopic.englishName) topicNames.push(ondTopic.englishName);
+    });
+    var searchLogs = SearchLog.find({
+        str: {$nin: topicNames},
+        count: {$gte: Config.searchKeywordFrequency}
+    }).fetch();
     if (searchLogs.length) {
         var emailConfig = EmailConfig.findOne({key: "keywordFrequency"});
         Email.send({
             to: Users.findOne({username: Config.sysAdmin}).emails[0].address,
             from: 'publish@scichina.org',
             subject: emailConfig ? emailConfig.subject : 'Search Keyword Frequency Reached',
-            html: JET.render('searchFrequency', {"searchLogs":searchLogs})
+            html: JET.render('searchFrequency', {"searchLogs": searchLogs})
 
         });
         searchLogs.forEach(function (entry) {
