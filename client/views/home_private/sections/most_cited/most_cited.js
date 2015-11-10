11c1f20a3be2065c591643796317f0b127121526
arthuryyx <arthuryyx@hotmail.com>
2015-11-10 17:05:08 +0800

    fix sort most cited on homepage and more page

--- client/views/home_private/sections/most_cited/most_cited.js
+++ client/views/home_private/sections/most_cited/most_cited.js
@@ -1,7 +1,7 @@
 Template.mostCitedArticleList.helpers({
     mostCitedArticles: function () {
-        if (this.journalId) return MostCited.find({journalId: this.journalId}, {sort: {count: 1}, limit: 5});
-        return MostCited.find({}, {sort: {count: 1}, limit: 5});
+        if (this.journalId) return MostCited.find({journalId: this.journalId}, {sort: {count: -1}, limit: 5});
+        return MostCited.find({}, {sort: {count: -1}, limit: 5});
     },
     mostCiteCount: function () {
         if (5 < MostCited.find().count()) {
