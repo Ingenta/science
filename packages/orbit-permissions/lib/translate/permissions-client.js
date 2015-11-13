2ae231341db48ad048d9f75b4c881d49f56f4a7f
蒋凯 <kai.jiang@digitalpublishing.cn>
2015-11-13 10:04:12 +0800

    translate coffee to js

---
+++ packages/orbit-permissions/lib/translate/permissions-client.js
@@ -0,0 +1,44 @@
+var OrbitPermissions, helpers;
+
+OrbitPermissions = share.OrbitPermissions;
+
+helpers = share.helpers;
+
+if (Package.templating) {
+	Package.templating.Template.registerHelper(globals.helper_name, function(permission, package_name) {
+		return OrbitPermissions.userCan(permission, package_name);
+	});
+}
+
+_.extend(OrbitPermissions, {
+	getPermissionsDescriptions: function() {
+		var fallback_language, language, permissions;
+		language = helpers.getLanguage();
+		fallback_language = helpers.getFallbackLanguage();
+		permissions = {};
+		this._loopPermissions(function(package_name, permission_name, permission_description) {
+			var description;
+			description = permission_description[fallback_language];
+			if (language in permission_description) {
+				description = permission_description[language];
+			}
+			return permissions[package_name + ":" + permission_name] = description;
+		});
+		return permissions;
+	},
+	getRolesDescriptions: function() {
+		var fallback_language, language, roles;
+		language = helpers.getLanguage();
+		fallback_language = helpers.getFallbackLanguage();
+		roles = {};
+		this._loopRoles(function(package_name, role_name, role_permissions, role_description) {
+			var description;
+			description = role_description[fallback_language];
+			if (language in role_description) {
+				description = role_description[language];
+			}
+			return roles[package_name + ":" + role_name] = description;
+		});
+		return roles;
+	}
+});
