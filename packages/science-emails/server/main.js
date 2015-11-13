4561dece3ed357866415da81854c3106c7d4e048
arthuryyx <arthuryyx@hotmail.com>
2015-11-13 15:02:44 +0800

    scp email banner

--- packages/science-emails/server/main.js
+++ packages/science-emails/server/main.js
@@ -16,9 +16,10 @@ Science.Email.searchFrequencyEmail = function () {
             to: Users.findOne({username: Config.sysAdmin}).emails[0].address,
             from: 'publish@scichina.org',
             subject: emailConfig ? emailConfig.subject : 'Search Keyword Frequency Reached',
-            html: JET.render('searchFrequency', {"searchLogs": searchLogs})
+            html: JET.render('searchFrequency', {"searchLogs": searchLogs, "scpbannerurl": Meteor.absoluteUrl()+ "email/logo.png"})
 
         });
+        console.log("email sent");
         searchLogs.forEach(function (entry) {
             SearchLog.update({_id: entry._id}, {$set: {count: 0}});
         });
