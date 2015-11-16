c3cd2d0d7029177aedae81af6e7401ae410ca6f4
Jack Kavanagh <jackkav@gmail.com>
2015-11-16 10:05:07 +0800

    logger object will log exceptions to /tmp/exception.log and debug messages to /tmp/debug.log, everything will also be logged to mongo in collection: log

--- packages/science-logger/package.js
+++ packages/science-logger/package.js
@@ -1,13 +1,6 @@
 Package.describe({
     name: 'science-logger',
-    version: '0.0.1',
-    // Brief, one-line summary of the package.
-    summary: '',
-    // URL to the Git repository containing the source code for this package.
-    git: '',
-    // By default, Meteor will default to using README.md for documentation.
-    // To avoid submitting documentation, set this field to null.
-    documentation: 'README.md'
+    version: '0.0.2',
 });
 Npm.depends({
     "winston": "2.1.0",
@@ -18,11 +11,4 @@ Package.onUse(function (api) {
     api.use('ecmascript');
     api.addFiles('science-logger.js');
     api.export("logger", "server");
-});
-
-Package.onTest(function (api) {
-    api.use('ecmascript');
-    api.use('tinytest');
-    api.use('science-logger');
-    api.addFiles('science-logger-tests.js');
 });
