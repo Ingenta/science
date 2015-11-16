4a71e2d286e70aa81c2f8eaf8711cb884de7da5e
Jack Kavanagh <jackkav@gmail.com>
2015-11-16 10:12:30 +0800

    move logger dependancy to science-lib

--- packages/solr/package.js
+++ packages/solr/package.js
@@ -1,28 +1,29 @@
 Package.describe({
-  name: 'jackjiang:solr',
-  version: '0.0.2',
-  // Brief, one-line summary of the package.
-  summary: 'Solr module for meteor',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+    name: 'jackjiang:solr',
+    version: '0.0.2',
+    // Brief, one-line summary of the package.
+    summary: 'Solr module for meteor',
+    // URL to the Git repository containing the source code for this package.
+    git: '',
+    // By default, Meteor will default to using README.md for documentation.
+    // To avoid submitting documentation, set this field to null.
+    documentation: 'README.md'
 });
 
-Package.onUse(function(api) {
-  api.versionsFrom('1.1.0.3');
+Package.onUse(function (api) {
+    api.versionsFrom('1.1.0.3');
 
-  api.use([
-      'http'
-  ],'server');
+    api.use([
+        'http',
+        'science-lib'
+    ], 'server');
 
-  api.addFiles('solr.js','server');
-  api.export('Solr','server');
+    api.addFiles('solr.js', 'server');
+    api.export('Solr', 'server');
 });
 
-Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('jackjiang:solr');
-  api.addFiles('solr-tests.js');
+Package.onTest(function (api) {
+    api.use('tinytest');
+    api.use('jackjiang:solr');
+    api.addFiles('solr-tests.js');
 });
