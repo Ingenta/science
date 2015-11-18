8def54ce20e590da001fe952c179dd712dc721a5
Jack Kavanagh <jackkav@gmail.com>
2015-11-18 10:47:24 +0800

    fixed bug where collections wouldn't show on publisher page

--- packages/science-collections/client/view/collectionsList/list.js
+++ packages/science-collections/client/view/collectionsList/list.js
@@ -1,54 +1,57 @@
 Template.collectionsList.helpers({
-	collections:function(){
-        var journalId = Session.get("currentJournalId");
-		var pubId = Session.get('filterPublisher');
-		var first = Session.get('firstLetter');
-		var numPerPage = Session.get('PerPage');
-		if(numPerPage === undefined){
-			numPerPage = 10;
-		}
-		var q = {};
-		pubId && (q.publisherId = pubId);
-        journalId &&(q.journalId = journalId);
+    collections: function () {
+        //dont use session journalid if this is a publisher page
+        var journalId
+        if (Router.current().route.getName() !== "publisher.name")
+            journalId = Session.get("currentJournalId");
+        var pubId = Session.get('filterPublisher');
+        var first = Session.get('firstLetter');
+        var numPerPage = Session.get('PerPage');
+        if (numPerPage === undefined) {
+            numPerPage = 10;
+        }
+        var q = {};
+        pubId && (q.publisherId = pubId);
+        journalId && (q.journalId = journalId);
         var reg;
-        if(first && first == "other"){
-            reg="^[^A-Z]"
-        }else{
+        if (first && first == "other") {
+            reg = "^[^A-Z]"
+        } else {
             reg = "^" + first;
         }
-		first && (q.title = {$regex:reg, $options: "i"});
-		return collPaginator.find(q,{itemsPerPage:numPerPage});
-	},
-	collectionPageCount:function(){
-		var journalId = Session.get("currentJournalId");
-		var pubId = Session.get('filterPublisher');
-		var first = Session.get('firstLetter');
-		var q = {};
-		pubId && (q.publisherId = pubId);
-		journalId &&(q.journalId = journalId);
-		var reg;
-		if(first && first == "other"){
-			reg="^[^A-Z]"
-		}else{
-			reg = "^" + first;
-		}
-		first && (q.title = {$regex:reg, $options: "i"});
-		return collPaginator.find(q).count()>10;
-	}
+        first && (q.title = {$regex: reg, $options: "i"});
+        return collPaginator.find(q, {itemsPerPage: numPerPage});
+    },
+    collectionPageCount: function () {
+        var journalId = Session.get("currentJournalId");
+        var pubId = Session.get('filterPublisher');
+        var first = Session.get('firstLetter');
+        var q = {};
+        pubId && (q.publisherId = pubId);
+        journalId && (q.journalId = journalId);
+        var reg;
+        if (first && first == "other") {
+            reg = "^[^A-Z]"
+        } else {
+            reg = "^" + first;
+        }
+        first && (q.title = {$regex: reg, $options: "i"});
+        return collPaginator.find(q).count() > 10;
+    }
 });
 
 Template.updateCollectionForm.helpers({
-	getTitle: function () {
-		return TAPi18n.__("Update");
-	}
+    getTitle: function () {
+        return TAPi18n.__("Update");
+    }
 })
 
 Template.oneCollection.events({
-	"click a.fa-trash":function(e){
-		e.preventDefault();
-		var collId = this._id;
-		confirmDelete(e,function(){
-			ArticleCollections.remove({_id: collId});
-		})
-	}
+    "click a.fa-trash": function (e) {
+        e.preventDefault();
+        var collId = this._id;
+        confirmDelete(e, function () {
+            ArticleCollections.remove({_id: collId});
+        })
+    }
 })
