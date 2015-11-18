e97f67f7d4c54c58498e290aed48e7138c903919
Jack Kavanagh <jackkav@gmail.com>
2015-11-18 13:41:40 +0800

    sort browse articles by location id descending

--- server/publish/articles/journal.js
+++ server/publish/articles/journal.js
@@ -1,5 +1,6 @@
 Meteor.publish('oneJournalArticles', function (id) {
     return Articles.find({journalId: id}, {
+        sort: {elocationId: 1},
         fields: {sections: 0, figures: 0, references: 0}
     });
 });
@@ -8,6 +9,6 @@ Meteor.publish('oneIssueArticlesByArticleId', function (id) {
     var art = Articles.findOne({_id: id});
     if(!art)return;
     return Articles.find({issueId: art.issueId}, {
-        fields: {doi: 1}
+        fields: {doi: 1, elocationId: 1}
     });
 });
