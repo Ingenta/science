d2555dc32f38243f31c053b92b7a615ad4562840
arthuryyx <arthuryyx@hotmail.com>
2015-11-23 17:59:15 +0800

    email this template

--- packages/science-emails/server/emails.js
+++ packages/science-emails/server/emails.js
@@ -37,24 +37,40 @@ Meteor.methods({
             if (Meteor.user().profile.realName)
                 user = Meteor.user().profile.realName;
 
-        if (values.reasons === undefined)values.reasons = "";
+        //if (values.reasons === undefined)values.reasons = "";
 
-        var reason = values.reasons + ' \n\n'
+        //var reason = values.reasons + ' \n\n'
+
+        //Images.findOne({_id: journal.banner}).url()
+        var article = Articles.findOne({
+            doi: values.doi
+        }, {
+            fields: {_id: 1, title: 1, authors: 1, year: 1, volume: 1, issue: 1, elocationId: 1, journalId: 1, 'journal.titleCn': 1}
+        });
+        article.url = values.url;
+        article.journal.url = Meteor.absoluteUrl(Science.URL.journalDetail(article.journalId).substring(1));
+
         var emailSubject = user + ' has sent you an article';
-        var emailBody = reason + 'Click the link below to check it out. \n\n' + values.url;
-
-        var emailThisContent = EmailConfig.findOne({key: "emailThis"});
-        if (emailThisContent) {
-            if (emailThisContent.subject)emailSubject = emailThisContent.subject;
-            if (emailThisContent.body)emailBody = emailThisContent.body + '\n\n' + emailBody;
-        }
+        //var emailBody = reason + 'Click the link below to check it out. \n\n' + values.url;
+        //var emailThisContent = EmailConfig.findOne({key: "emailThis"});
+        //if (emailThisContent) {
+        //    if (emailThisContent.subject)emailSubject = emailThisContent.subject;
+        //    if (emailThisContent.body)emailBody = emailThisContent.body + '\n\n' + emailBody;
+        //}
 
         Meteor.defer(function () {
             Email.send({
                 to: values.recipient,
                 from: 'publish@scichina.org',
                 subject: emailSubject,
-                text: emailBody
+                html: JET.render('emailThis', {
+                    "user": user,
+                    "reason": values.reasons,
+                    "articleList": [article],
+                    "scpLogoUrl": Config.rootUrl + "email/logo.png",
+                    "emailIcoUrl": Config.rootUrl + "email/ico.png",
+                    "rootUrl": Config.rootUrl
+                })
             });
         });
 
