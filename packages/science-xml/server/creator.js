1e2f6e9f0dcc7e9f89dd529c7f8e895db674d155
Jack Kavanagh <jackkav@gmail.com>
2015-11-23 14:45:34 +0800

    fixed volume id setting

--- packages/science-xml/server/creator.js
+++ packages/science-xml/server/creator.js
@@ -8,8 +8,8 @@ ScienceXML.IssueCreator = function () {
 		if (obj.journalId && obj.volume && obj.issue) {
 			if (!tasks.exists(obj)) {
 				tasks.push(obj);
-				var volume = Volumes.upsert({journalId: obj.journalId, volume: obj.volume},{journalId: obj.journalId, volume: obj.volume});
-
+				 Volumes.upsert({journalId: obj.journalId, volume: obj.volume},{journalId: obj.journalId, volume: obj.volume});
+				var volume = Volumes.findOne({journalId: obj.journalId, volume: obj.volume})._id;
 				var issue = Issues.findOne({journalId: obj.journalId, volume: obj.volume, issue: obj.issue});
 				issue     = (issue && issue._id) || Issues.insert({
 						journalId: obj.journalId,
