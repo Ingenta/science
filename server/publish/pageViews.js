4175fd65e1640e2c47befacbf3b88b320dd8b82c
Jack Kavanagh <jackkav@gmail.com>
2015-11-25 00:13:58 +0800

    moved publish files into folders by use

--- server/publish/pageViews.js
+++ server/publish/pageViews.js
@@ -1,6 +1,3 @@
-Meteor.publish('articleViews', function() {
-    return PageViews.find();
-});
 Meteor.publish('articleViewsByArticleId', function(id) {
     return PageViews.find({articleId:id});
 });
