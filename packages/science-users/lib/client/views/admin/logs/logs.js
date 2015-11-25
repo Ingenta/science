aed0e58ab1895523e1579843cb4daf114a50a2e5
Jack Kavanagh <jackkav@gmail.com>
2015-11-25 15:47:25 +0800

    improved logging page

--- packages/science-users/lib/client/views/admin/logs/logs.js
+++ packages/science-users/lib/client/views/admin/logs/logs.js
@@ -4,6 +4,11 @@ Template.AdminLogs.helpers({
     },
     metaToString: function (meta) {
         return JSON.stringify(meta, null, '\t');
+    },
+    getLevelClass: function(level){
+        if (level==="error") return "bg-danger";
+        if (level==="warn") return "bg-warning";
+        return "";
     }
 });
 Template.AdminLogs.events({
