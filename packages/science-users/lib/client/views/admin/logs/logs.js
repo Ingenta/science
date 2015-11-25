eebc029924ed059dc1eef6fb8e29632ea0790ab2
Jack Kavanagh <jackkav@gmail.com>
2015-11-25 15:30:28 +0800

    added popover with metadata about error

--- packages/science-users/lib/client/views/admin/logs/logs.js
+++ packages/science-users/lib/client/views/admin/logs/logs.js
@@ -1,5 +1,13 @@
 Template.AdminLogs.helpers({
     recentLogs: function () {
         return Logs.find();
+    },
+    metaToString: function (meta) {
+        return JSON.stringify(meta, null, '\t');
+    }
+});
+Template.AdminLogs.events({
+    'click .metaLogPopOver': function (e) {
+        $(e.target).popover('show')
     }
 });
