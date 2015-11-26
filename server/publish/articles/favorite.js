51c0b74d865bae490056d25eeb2e6204423ff0bd
Jack Kavanagh <jackkav@gmail.com>
2015-11-26 17:14:39 +0800

    small optimisation

--- server/publish/articles/favorite.js
+++ server/publish/articles/favorite.js
@@ -1,40 +1,48 @@
-Meteor.publish('myFavouriteArticles', function (userId) {
+Meteor.publish('myFavouriteArticles', function () {
     var favorites = [];
-    var thisUser = Users.findOne({_id: userId});
-    if (thisUser)
-        if (thisUser.favorite)
-            favorites = _.pluck(thisUser.favorite, "articleId")
-    return [
-        Articles.find({_id: {$in: favorites}}, {
-            fields: articleWithMetadata
-        }),
-        Publishers.find({}, {
-            fields: {name: 1}
-        }),
-        Publications.find({}, {
-            fields: {publisher: 1, shortTitle: 1, title: 1, titleCn: 1}
-        })
-    ]
-
+    if (this.userId) {
+        var thisUser = Users.findOne({_id: this.userId});
+        if (thisUser)
+            if (thisUser.favorite)
+                favorites = _.pluck(thisUser.favorite, "articleId")
+        return [
+            Articles.find({_id: {$in: favorites}}, {
+                fields: articleWithMetadata
+            }),
+            Publishers.find({}, {
+                fields: {name: 1}
+            }),
+            Publications.find({}, {
+                fields: {publisher: 1, shortTitle: 1, title: 1, titleCn: 1}
+            })
+        ]
+    }
+    else {
+        this.ready();
+    }
 });
 
-Meteor.publish('myWatchedArticles', function (userId) {
+Meteor.publish('myWatchedArticles', function () {
     var watched = [];
-    var thisUser = Users.findOne({_id: userId});
-    if (thisUser)
-        if (thisUser.profile)
-            if (thisUser.profile.articlesOfInterest)
-                watched = thisUser.profile.articlesOfInterest;
-    return [
-        Articles.find({_id: {$in: watched}}, {
-            fields: articleWithMetadata
-        }),
-        Publishers.find({}, {
-            fields: {name: 1}
-        }),
-        Publications.find({}, {
-            fields: {publisher: 1, shortTitle: 1}
-        })
-    ]
-
+    if (this.userId) {
+        var thisUser = Users.findOne({_id: this.userId});
+        if (thisUser)
+            if (thisUser.profile)
+                if (thisUser.profile.articlesOfInterest)
+                    watched = thisUser.profile.articlesOfInterest;
+        return [
+            Articles.find({_id: {$in: watched}}, {
+                fields: articleWithMetadata
+            }),
+            Publishers.find({}, {
+                fields: {name: 1}
+            }),
+            Publications.find({}, {
+                fields: {publisher: 1, shortTitle: 1}
+            })
+        ]
+    }
+    else {
+        this.ready();
+    }
 });
