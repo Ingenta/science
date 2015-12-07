3d8106a31e0a9fca60cba72342f5ea424666a249
arthuryyx <arthuryyx@hotmail.com>
2015-12-07 14:17:52 +0800

    add journal news permission change homepage advertisement permission defination

--- packages/science-permissions/group/news.js
+++ packages/science-permissions/group/news.js
@@ -10,7 +10,7 @@ newsPermissions
     })
     .definePermission("edit-homepage-news", {
         en: {name: "edit homepage news", summary: "edit homepage news"},
-        cn: {name: "修改新闻管理", summary: "修改首页新闻"},
+        cn: {name: "修改首页新闻", summary: "修改首页新闻"},
         options: {
             level: adminLevel
         }
@@ -21,19 +21,102 @@ newsPermissions
         options: {
             level: adminLevel
         }
+    })
+    //.definePermission("add-publisher-news", {
+    //    en: {name: "add publisher news", summary: "add publisher news"},
+    //    cn: {name: "新建出版商新闻", summary: "新建出版商新闻"},
+    //    options: {
+    //        level: publisherLevel
+    //    }
+    //})
+    //.definePermission("edit-publisher-news", {
+    //    en: {name: "edit publisher news", summary: "edit publisher news"},
+    //    cn: {name: "修改出版商新闻", summary: "修改出版商新闻"},
+    //    options: {
+    //        level: publisherLevel
+    //    }
+    //})
+    //.definePermission("delete-publisher-news", {
+    //    en: {name: "delete publisher news", summary: "delete publisher news"},
+    //    cn: {name: "删除出版商新闻", summary: "删除出版商新闻"},
+    //    options: {
+    //        level: publisherLevel
+    //    }
+    //})
+    .definePermission("add-journal-news", {
+        en: {name: "add journal news", summary: "add journal news"},
+        cn: {name: "新建期刊新闻", summary: "新建期刊新闻"},
+        options: {
+            level: journalLevel
+        }
+    })
+    .definePermission("edit-journal-news", {
+        en: {name: "edit journal news", summary: "edit journal news"},
+        cn: {name: "修改期刊新闻", summary: "修改期刊新闻"},
+        options: {
+            level: journalLevel
+        }
+    })
+    .definePermission("delete-journal-news", {
+        en: {name: "delete journal news", summary: "delete journal news"},
+        cn: {name: "删除期刊新闻", summary: "删除期刊新闻"},
+        options: {
+            level: journalLevel
+        }
     });
 
 newsPermissions
-    .defineRole("news-manager", ["add-homepage-news", "edit-homepage-news", "delete-homepage-news"], {
+    .defineRole("news-manager-homepage",
+        [
+            "add-homepage-news",
+            "edit-homepage-news",
+            "delete-homepage-news"
+        ], {
         en: {
-            name: "news manager",
-            summary: "manager for news"
+            name: "homepage news manager",
+            summary: "manager for homepage news"
         },
         cn: {
-            name: "新闻管理",
-            summary: "可以增删改新闻信息的角色"
+            name: "首页新闻管理",
+            summary: "可以增删改首页新闻信息的角色"
         },
         options: {
             level: adminLevel
+        }
+    })
+    //.defineRole("news-manager-publisher",
+    //    [
+    //        "add-publisher-news",
+    //        "edit-publisher-news",
+    //        "delete-publisher-news"
+    //    ], {
+    //    en: {
+    //        name: "publisher news manager",
+    //        summary: "manager for news publisher"
+    //    },
+    //    cn: {
+    //        name: "出版商新闻管理",
+    //        summary: "可以增删改出版商新闻信息的角色"
+    //    },
+    //    options: {
+    //        level: publisherLevel
+    //    }
+    //})
+    .defineRole("news-manager-journal",
+        [
+            "add-journal-news",
+            "edit-journal-news",
+            "delete-journal-news"
+        ], {
+        en: {
+            name: "journal news manager",
+            summary: "manager for journal news"
+        },
+        cn: {
+            name: "期刊新闻管理",
+            summary: "可以增删改期刊新闻信息的角色"
+        },
+        options: {
+            level: journalLevel
         }
     });
