0991c6593134f2ba0d224bab8fc0bc6bf08f54c3
Jack Kavanagh <jackkav@gmail.com>
2015-12-08 19:01:16 +0800

    added graphics magic

--- both/collections/article/figuresStore.js
+++ both/collections/article/figuresStore.js
@@ -1,5 +1,14 @@
+var createThumb = function(fileObj, readStream, writeStream) {
+    // Transform the image into a 10x10px thumbnail
+    gm(readStream, fileObj.name()).resize('500', '500').stream().pipe(writeStream);
+};
 FiguresStore = new FS.Collection("figures", {
-    stores: [new FS.Store.FileSystem("figures", {})],
+    stores: [new FS.Store.FileSystem("figures", { transformWrite: createThumb })],
+    filter: {
+        allow: {
+            contentTypes: ['image/*'] //allow only images in this FS.Collection
+        }
+    },
     path: Config.staticFiles.uploadFiguresDir
 });
 FiguresStore.allow({
