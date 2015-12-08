a2926cc51debc9411473357322597d7d9a6aed34
xiancui <451763649@qq.com>
2015-12-08 11:50:36 +0800

    update

--- both/lib/0_config.js
+++ both/lib/0_config.js
@@ -109,7 +109,6 @@ Config = {
         + "Downloaded to IP: {ip} On: {time} {url}"
     },
     rootUrl: Meteor.absoluteUrl(),
-    mainPublish: "hSsscs85HXuu2qTfJ",
     searchKeywordFrequency: 3000,
     sysAdmin: "admin",
     parser: {
