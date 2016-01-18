9df8a200faa4fa9e3ed74b3af2c595a1c5c8ad44
Jack Kavanagh <jackkav@gmail.com>
2016-01-18 16:28:29 +0800

    got topic edit and delete buttons working

--- packages/science-topics/client/views/topics.js
+++ packages/science-topics/client/views/topics.js
@@ -1,21 +1,33 @@
 Template.Topics.onRendered(function () {
+    Session.set("selectedTopic", null);
     var isLangCn = TAPi18n.getLanguage() === "zh-CN";
-    $('#tree').treeview({levels: 1, showBorder: false, data: flatTopicsToTreeNodes(isLangCn)});
+    $('#tree').treeview({
+        levels: 1,
+        showBorder: false,
+        data: flatTopicsToTreeNodes(isLangCn),
+        showTags: true,
+        onNodeSelected: function (event, node) {
+            Session.set("selectedTopic", node.tags[0]);
+        },
+        onNodeUnselected: function (event, node) {
+            Session.set("selectedTopic", null);
+        }
+    });
 })
-flatTopicsToTreeNodes = function(isLangCn){
+flatTopicsToTreeNodes = function (isLangCn) {
     var topicTree = [];
     _.each(Topics.find({parentId: null}).fetch(), function (topic) {
-        var thisTopic = {text: isLangCn ? topic.name : topic.englishName};
+        var thisTopic = {text: isLangCn ? topic.name : topic.englishName, tags: [topic._id]};
         var childTopics = [];
         _.each(Topics.find({parentId: topic._id}).fetch(), function (child) {
-            var oneChild = {text: isLangCn ? child.name : child.englishName};
+            var oneChild = {text: isLangCn ? child.name : child.englishName, tags: [child._id]};
             var grandchildTopics = [];
             _.each(Topics.find({parentId: child._id}).fetch(), function (grandchild) {
-                var oneGrandchild = {text: isLangCn ? grandchild.name : grandchild.englishName};
+                var oneGrandchild = {text: isLangCn ? grandchild.name : grandchild.englishName, tags: [grandchild._id]};
                 grandchildTopics.push(oneGrandchild)
             })
             if (!_.isEmpty(grandchildTopics))
-            oneChild.nodes = grandchildTopics;
+                oneChild.nodes = grandchildTopics;
             childTopics.push(oneChild)
         })
         if (!_.isEmpty(childTopics))
@@ -24,9 +36,42 @@ flatTopicsToTreeNodes = function(isLangCn){
     })
     return topicTree;
 }
+Template.Topics.helpers({
+    selectedTopic: function () {
+        return Session.get("selectedTopic");
+    },
+    getAddNewStr: function () {
+        return TAPi18n.__("Add new");
+    },
+    hasNoChildTopics: function () {
+        var topicId = Session.get("selectedTopic");
+        if(!topicId)return true;
+        if (Topics.findOne({parentId: topicId}))
+            return false;
+        return true;
+    },
+    getDoc: function () {
+        var topicId = Session.get("selectedTopic");
+        if(!topicId)return;
+        return Topics.findOne({_id: topicId});
+    }
+});
+
+AutoForm.addHooks(['addTopicModalForm'], {
+    onSuccess: function () {
+        FlashMessages.sendSuccess(TAPi18n.__("Success"), {hideDelay: 3000});
+    },
+    before: {
+        insert: function (doc) {
+            doc.parentId = Session.get('selectedTopic');
+            return doc;
+        }
+    }
+}, true);
+
 //var reactDic = new ReactiveDict();
 //
-
+//
 //Template.Topics.onCreated(function(){
 //    reactDic.set("canAdd",Permissions.userCan("add-topic","topic",Meteor.userId()));
 //    reactDic.set("canModify",Permissions.userCan("modify-topic","topic",Meteor.userId()));
