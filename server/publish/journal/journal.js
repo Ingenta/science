f9e66825f665758ead6d9be63a682db213d6ef6c
Jack Kavanagh <jackkav@gmail.com>
2016-03-15 16:25:40 +0800

    added some new journal page publishing methods

--- server/publish/journal/journal.js
+++ server/publish/journal/journal.js
@@ -14,3 +14,99 @@ Meteor.publish('oneIssueArticlesByArticleId', function (id) {
         fields: {doi: 1, elocationId: 1, issueId: 1}
     });
 });
+
+Meteor.publish('journalBrowseTab', function (journalShortTitle,issue) {
+    if(!journalShortTitle)return this.ready();
+    var journal = Publications.findOne({shortTitle: journalShortTitle});
+    if(!journal)return this.ready();
+    var journalId=journal._id;
+    if(!issue)return this.ready();
+
+    //combine all historical journals
+    var idArr = [journalId];
+    var journal=Publications.findOne({_id:journalId},{fields:{historicalJournals:1}});
+
+    if(journal && !_.isEmpty(journal.historicalJournals)){
+        idArr = _.union(idArr,journal.historicalJournals)
+    }
+    return [
+        Articles.find({journalId: journalId, issue: issue}, {
+            fields: {sections: 0, figures: 0, references: 0}
+        }),
+        Volumes.find({journalId:{$in:idArr}}),
+        Issues.find({journalId:{$in:idArr}})
+      ]
+});
+Meteor.publish('journalBrowseTabVolumeList', function (journalShortTitle) {
+    if(!journalShortTitle)return this.ready();
+    var journal = Publications.findOne({shortTitle: journalShortTitle});
+    if(!journal)return this.ready();
+    var journalId=journal._id;
+
+    //combine all historical journals
+    var idArr = [journalId];
+    var journal=Publications.findOne({_id:journalId},{fields:{historicalJournals:1}});
+
+    if(journal && !_.isEmpty(journal.historicalJournals)){
+        idArr = _.union(idArr,journal.historicalJournals)
+    }
+    return [
+        Volumes.find({journalId:{$in:idArr}}),
+        Issues.find({journalId:{$in:idArr}})
+    ]
+});
+Meteor.publish('journalBrowseTabArticleList', function (journalShortTitle, issueId) {
+    if(!journalShortTitle)return this.ready();
+    var journal = Publications.findOne({shortTitle: journalShortTitle});
+    if(!journal)return this.ready();
+    var journalId=journal._id;
+    if(!issueId)return this.ready();
+    return [
+        Articles.find({journalId: journalId, issueId: issueId}, {
+            fields: {sections: 0, figures: 0, references: 0, authorNotes:0, affiliations:0, tables:0, pacs:0, fundings:0}
+        })
+    ]
+});
+
+
+
+Meteor.publish('journalOverviewTab', function (journalShortTitle) {
+    if(!journalShortTitle)return this.ready();
+    var journal = Publications.findOne({shortTitle: journalShortTitle});
+    if(!journal)return this.ready();
+    var journalId=journal._id;
+    var recommended = EditorsRecommend.find({publications: journalId}, {fields: {ArticlesId: 1}}, {limit: 5}).fetch();
+    var recommendedArticleIds = _.pluck(recommended, "ArticlesId");
+    var mostRead = createMostReadList(journalId, 5);
+    var homepageArticles = _.union(recommendedArticleIds,mostRead);
+    return [
+        Articles.find({_id: {$in: homepageArticles}}, {
+            fields: articleWithMetadata
+        }),
+        Publishers.find({}, {
+            fields: {shortname: 1}
+        }),
+        Publications.find({}, {
+            fields: {publisher: 1, shortTitle: 1, title: 1, titleCn: 1}
+        }),
+        SuggestedArticles.find()
+    ]
+
+});
+
+
+Meteor.publish('journalOnlineFirstTab',function (journalShortTitle) {
+    if(!journalShortTitle)return this.ready();
+    var journal = Publications.findOne({shortTitle: journalShortTitle});
+    if(!journal)return this.ready();
+    var journalId=journal._id;
+    return Articles.find({journalId:journalId,pubStatus:"online_first"});
+})
+Meteor.publish('journalAcceptedTab',function (journalShortTitle) {
+    if(!journalShortTitle)return this.ready();
+    var journal = Publications.findOne({shortTitle: journalShortTitle});
+    if(!journal)return this.ready();
+    var journalId=journal._id;
+    return Articles.find({journalId:journalId,pubStatus:"accepted"});
+})
+
