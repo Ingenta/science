d3bb9ad4b979e1366072c24ae21998eb8da7ae0b
Jack Kavanagh <jackkav@gmail.com>
2016-03-15 10:31:44 +0800

    fixed three time refresh on journal page

--- server/publish/journal/journal.js
+++ server/publish/journal/journal.js
@@ -1,4 +1,6 @@
 Meteor.publish('oneJournalArticles', function (id,issueId) {
+    if(!id)return this.ready();
+    if(!issueId)return this.ready();
     return Articles.find({journalId: id, issueId: issueId}, {
         fields: {sections: 0, figures: 0, references: 0}
     });
@@ -6,7 +8,7 @@ Meteor.publish('oneJournalArticles', function (id,issueId) {
 
 Meteor.publish('oneIssueArticlesByArticleId', function (id) {
     var art = Articles.findOne({_id: id});
-    if(!art)return;
+    if(!art)return this.ready();
     return Articles.find({issueId: art.issueId}, {
         fields: {doi: 1, elocationId: 1, issueId: 1}
     });
