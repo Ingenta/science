e2fe16c6b6f597c6cd2f23b35fc9983e462c2e90
蒋凯 <kai.jiang@digitalpublishing.cn>
2016-03-23 17:39:36 +0800

    中文预出版数据解析支持

--- packages/science-xml/server/acceptedParse.js
+++ packages/science-xml/server/acceptedParse.js
@@ -5,8 +5,8 @@ Science.parserAccepted = function(filepath){
     var parse = function (data) {
         var article = {};
         dom = new Science.Dom().parseFromString(data);
-        article.issn = parserHelper.getSimpleVal("//journal/issn[@issn_type='print']", dom);
-        article.doi = parserHelper.getSimpleVal("//journal/doi", dom);
+        article.issn = parserHelper.getSimpleVal("//journal/issn[@issn_type='print'] | //article/journal_issn", dom);
+        article.doi = parserHelper.getSimpleVal("//journal/doi | //article/doi", dom);
         if(article.doi)
             article.articledoi = article.doi.split("/")[1];
         article.title = parserHelper.getMultiVal("//article/article_title", dom);
@@ -20,8 +20,8 @@ Science.parserAccepted = function(filepath){
         _.each(authorNodes, function (authorNode) {
             var author = {affs: []};
             var fullName = {};
-            author.surname = parserHelper.getMultiVal("child::first_name", authorNode);
-            author.given = parserHelper.getMultiVal("child::last_name", authorNode);
+            author.surname = parserHelper.getMultiVal("child::last_name", authorNode);
+            author.given = parserHelper.getMultiVal("child::first_name", authorNode);
             fullName.en = author.surname.en + " " + author.given.en;
             fullName.cn = author.surname.cn + " " + author.given.cn;
             author.fullname=fullName;
@@ -58,18 +58,42 @@ Science.parserAccepted = function(filepath){
 
         article.abstract = parserHelper.getMultiVal("//article/abstract", dom);
         article.fundings = parserHelper.getFirstAttribute("//configurable_data_fields/custom_fields[@cd_code='Funding source']/attribute::cd_value", dom);
-        article.contentType = parserHelper.getSimpleVal("//article/publication_type", dom);
-        var year = parserHelper.getSimpleVal("//ms_id/rev_id[text()=0]/parent::ms_id/submitted_date/year", dom);
-        var month = parserHelper.getSimpleVal("//ms_id/rev_id[text()=0]/parent::ms_id/submitted_date/month", dom);
-        var day = parserHelper.getSimpleVal("//ms_id/rev_id[text()=0]/parent::ms_id/submitted_date/day", dom);
-        if(year && month && day){
-            article.received = new Date(Date.parse(year + '/ ' + month + '/' + day));
+        article.contentType = parserHelper.getSimpleVal("//article/publication_type | //article/manuscript_type", dom);
+
+        var recDateStr = parserHelper.getSimpleVal("//received_date",dom);
+        if(recDateStr){
+            article.received = new Date(Date.parse(recDateStr));
+        }else{
+            var year = parserHelper.getSimpleVal("//ms_id/rev_id[text()=0]/parent::ms_id/submitted_date/year", dom);
+            var month = parserHelper.getSimpleVal("//ms_id/rev_id[text()=0]/parent::ms_id/submitted_date/month", dom);
+            var day = parserHelper.getSimpleVal("//ms_id/rev_id[text()=0]/parent::ms_id/submitted_date/day", dom);
+            if(year && month && day){
+                article.received = new Date(Date.parse(year + '/ ' + month + '/' + day));
+            }
         }
-        article.accepted = parserHelper.getFirstAttribute("//task[@name='EM Final Decision']/status/attribute::event_date", dom);
-        if(article.accepted)
-            article.accepted = article.accepted.split("T")[0];
-        article.topic = parserHelper.getFirstAttribute("//content/attr_type[@name='Speciality']/attribute/attribute::name", dom);
+        var acceptedDateStr = parserHelper.getFirstAttribute("//task[@name='EM Final Decision']/status/attribute::event_date | //task[@name='AE Make Decision']/status/attribute::event_date", dom);
+        if(acceptedDateStr){
+            article.accepted = new Date(Date.parse(article.accepted.split("T")[0]));
+        }else{
+            acceptedDateStr = parserHelper.getSimpleVal("//publish_date",dom)
+            article.accepted = new Date(Date.parse(acceptedDateStr));
+        }
+
+        var topicStr = parserHelper.getFirstAttribute("//content/attr_type[@name='Speciality']/attribute/attribute::name", dom);
+        if(!topicStr){
+            topicStr = parserHelper.getSimpleVal("//article/manuscript_fieldtwo",dom);
+        }
+        if(topicStr){
+            var t=Topics.findOne({$or:[{name:topicStr},{englishName:topicStr}]});
+            if(t)
+                article.topic= [t._id];
+        }
         var keywords = parserHelper.getAttributes("//content/attr_type[@name='Keywords']/attribute/attribute::name", dom);
+        if(!keywords){
+            var kwStr = parserHelper.getSimpleVal("//article/key_word", dom);
+            if(_.isString(kwStr))
+                keywords = kwStr.split(/,\s?/);
+        }
         article.keywords = {en: keywords,cn: keywords};
         if(article.issn){
             article.issn = article.issn.replace("-","");
