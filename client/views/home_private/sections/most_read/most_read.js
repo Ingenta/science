707a16a6204785cbf2cfa12df2a2076f342c5ba4
Jack Kavanagh <jackkav@gmail.com>
2016-03-31 19:33:34 +0800

    most read is now only called once

--- client/views/home_private/sections/most_read/most_read.js
+++ client/views/home_private/sections/most_read/most_read.js
@@ -1,26 +1,36 @@
+Template.mostReadArticleList.onCreated(function(){
+    if(Router.current().route.getName() === "home")
+    {
+        if(!Session.get("homePageMostReadArticleIds")) {
+            Meteor.call("getMostRead", undefined, 6, function (err, result) {
+                Session.set("homePageMostReadArticleIds", result);
+            });
+        }
 
+    }
+    else if(this.journalId){
+        var journalId = this.journalId;
+        if(!Session.get("mostReadIds"+journalId)) {
+            Meteor.call("getMostRead", journalId, 6, function (err, result) {
+                Session.set("mostReadIds" + journalId, result);
+            });
+        }
+    }
+})
 Template.mostReadArticleList.helpers({
     mostReadArticlesTopFive: function () {
         //NOTE: get 6 of each so that more button will show if more than 6 exist
         //homepage
         if(Router.current().route.getName() === "home")
         {
-            if(!Session.get("homePageMostReadArticleIds")) {
-                Meteor.call("getMostRead", undefined, 6, function (err, result) {
-                    Session.set("homePageMostReadArticleIds", result);
-                });
-            }
+            if(!Session.get("homePageMostReadArticleIds")) return;
             return _.map(Session.get("homePageMostReadArticleIds"), function (id) {
                 return Articles.findOne({_id: id})
             });
         }
         else if(this.journalId){
             var journalId = this.journalId;
-            if(!Session.get("mostReadIds"+journalId)) {
-                Meteor.call("getMostRead", journalId, 6, function (err, result) {
-                    Session.set("mostReadIds" + journalId, result);
-                });
-            }
+            if(!Session.get("mostReadIds"+journalId)) return;
             return _.map(Session.get("mostReadIds"+journalId), function (id) {
                 return Articles.findOne({_id: id})
             });
