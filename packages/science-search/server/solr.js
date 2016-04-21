84b5efcad0e08a21a373f25bc7c25ff5d93db0b8
Jack Kavanagh <jackkav@gmail.com>
2016-04-21 12:17:42 +0800

    lower case all search values

--- packages/science-search/server/solr.js
+++ packages/science-search/server/solr.js
@@ -48,7 +48,7 @@ SolrUtils = {
         if (queryArr) {
             var clearReg=/[&\/\\#,+()$~%.'":*?!<>^{}\[\]]/g;
             if (typeof queryArr === 'string')
-                return queryArr.replace(clearReg," ").trim();
+                return queryArr.replace(clearReg," ").trim().toLowerCase();
             qstring = "";
             var isFirstOne = true;
             _.each(queryArr, function (sQuery) {
@@ -58,7 +58,7 @@ SolrUtils = {
                 isFirstOne = false;
                 var solrFields = SolrUtils.fieldMap[sQuery.key];
                 var subQueues = _.map(solrFields, function (sField) {
-                    return sField + ":(" + sQuery.val.replace(clearReg," ").trim() + ")";
+                    return sField + ":(" + sQuery.val.replace(clearReg," ").trim().toLowerCase() + ")";
                 });
                 qstring += "(" + subQueues.join(" OR ") + ")";
             })
