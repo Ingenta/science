4d7c82086e0a0579217e5ab292a91fb8cda74b47
蒋凯 <kai.jiang@digitalpublishing.cn>
2016-05-06 11:01:17 +0800

    非MOOP期刊的文章的附录信息折叠信息标题改为附录

--- client/views/publications/articles/sections/moop.js
+++ client/views/publications/articles/sections/moop.js
@@ -15,6 +15,9 @@ Template.moopDetails.helpers({
     },
     moopDatas: function () {
         return moopCache.get("moop_" + this._id);
+    },
+    getName: function(){
+        return _.contains(Router.current().data().tabSelections,"MOOP")?TAPi18n.__("MOOP"):TAPi18n.__("Appendix")
     }
 })
 
