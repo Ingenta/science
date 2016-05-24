3a52448b52d650341f4a64f00b7dac16bdbd9162
xiancui <451763649@qq.com>
2016-05-24 11:12:36 +0800

    小平台作者中心跳转大平台路径修改

--- client/views/miniplatform/authorCenter/authorCenter.js
+++ client/views/miniplatform/authorCenter/authorCenter.js
@@ -4,13 +4,10 @@ Template.authorCentered.helpers({
         if (publisher)return Publications.find({publisher: publisher._id, visible:"1"},{sort: {title: 1}});
     },
     urlToJournalAuthorCenter: function (id) {
-        if(id){
-            var journal = Publications.findOne({_id: id});
-
-            if(journal){
-                var publisher = Publishers.findOne({_id: journal.publisher});
-                return "/publisher/"+publisher.shortname+"/journal/"+journal.shortTitle+"?slug=Author Center";
-            }
-        }
+        var journal = Publications.findOne({_id: id}, {fields: {shortTitle: 1, publisher: 1}});
+        if (!journal)return;
+        var pub = Publishers.findOne({_id: journal.publisher}, {fields: {shortname: 1}});
+        if (!pub)return;
+        return "http://engine.scichina.com/publisher/" + pub.shortname + "/journal/" + journal.shortTitle+"?slug=Author Center";
     }
 });
