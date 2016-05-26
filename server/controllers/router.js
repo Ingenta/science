4934120e99db9d6ab7b466102df36f3f68456aed
蒋凯 <kai.jiang@digitalpublishing.cn>
2016-05-26 13:12:26 +0800

    引用出口处增加对作者字段是否为空的检查，避免出现用户导出无作者的文章的引用出口信息时，服务器崩溃重启的问题

--- server/controllers/router.js
+++ server/controllers/router.js
@@ -20,16 +20,20 @@ Router.map(function () {
             var text = undefined;
             if (obj.language == 1) {
                 text = (obj.title.en || obj.title.cn) + "\n";
-                obj.authors.forEach(function (author) {
-                    text += (author.given.en || author.given.cn) + ", " + (author.surname.en || author.surname.cn) + " and ";
-                });
+                if(!_.isEmpty(obj.authors)){
+                    obj.authors.forEach(function (author) {
+                        text += (author.given.en || author.given.cn) + ", " + (author.surname.en || author.surname.cn) + " and ";
+                    });
+                }
                 text = text.substring(0, text.length - 5);
                 text += ", " + (obj.journal.title || obj.journal.titleCn) + ", ";
             } else {
                 text = (obj.title.cn || obj.title.en) + "\n";
-                obj.authors.forEach(function (author) {
-                    text += (author.given.cn || author.given.en) + ", " + (author.surname.cn || author.surname.en) + " and ";
-                });
+                if(!_.isEmpty(obj.authors)){
+                    obj.authors.forEach(function (author) {
+                        text += (author.given.cn || author.given.en) + ", " + (author.surname.cn || author.surname.en) + " and ";
+                    });
+                }
                 text = text.substring(0, text.length - 5);
                 text += ", " + (obj.journal.titleCn || obj.journal.title) + ", ";
             }
@@ -58,14 +62,18 @@ Router.map(function () {
             var name1 = "publisher/" + publisher.name + "/journal/" + obj.journal.title + "/" + obj.volume + "/" + obj.issue + "/" + obj.doi;
             var text = "@article{:/" + name1 + ",\n   author = \"";
             if (obj.language == 1) {
-                obj.authors.forEach(function (author) {
-                    text += (author.given.en || author.given.cn) + ", " + (author.surname.en || author.surname.cn) + " and ";
-                });
+                if(!_.isEmpty(obj.authors)){
+                    obj.authors.forEach(function (author) {
+                        text += (author.given.en || author.given.cn) + ", " + (author.surname.en || author.surname.cn) + " and ";
+                    });
+                }
                 text = text.substring(0, text.length - 5) + "\",\n   title = \"" + obj.title.en + "\",\n   journal = \"" + obj.journal.title;
             } else {
-                obj.authors.forEach(function (author) {
-                    text += (author.given.cn || author.given.en) + ", " + (author.surname.cn || author.surname.en) + " and ";
-                });
+                if(!_.isEmpty(obj.authors)) {
+                    obj.authors.forEach(function (author) {
+                        text += (author.given.cn || author.given.en) + ", " + (author.surname.cn || author.surname.en) + " and ";
+                    });
+                }
                 text = text.substring(0, text.length - 5) + "\",\n   title = \"" + obj.title.cn + "\",\n   journal = \"" + obj.journal.titleCn;
             }
             text += "\",\n   year = \"" + obj.year + "\",\n   volume = \"" + obj.volume + "\",\n   number = \"" + obj.issue + "\",\n   eid = " + (obj.elocationId || "") + ",\n   pages = \"" + (obj.startPage || "") + "-" + (obj.endPage || "") + "\",\n   url = \"" + Config.rootUrl + name1 + "\",\n   doi = \"http://dx.doi.org/" + obj.doi + "\" \n}\n\n";
@@ -93,14 +101,18 @@ Router.map(function () {
             var name1 = "publisher/" + publisher.name + "/journal/" + obj.journal.title + "/" + obj.volume + "/" + obj.issue + "/" + obj.doi;
             var text = "%0 Journal Article\n";
             if (obj.language == 1) {
-                obj.authors.forEach(function (author) {
-                    text += "%A " + (author.given.en || author.given.cn) + ", " + (author.surname.en || author.surname.cn) + "\n";
-                });
+                if(!_.isEmpty(obj.authors)) {
+                    obj.authors.forEach(function (author) {
+                        text += "%A " + (author.given.en || author.given.cn) + ", " + (author.surname.en || author.surname.cn) + "\n";
+                    });
+                }
                 text += "%T " + (obj.title.en || obj.title.cn) + "\n%D " + obj.year + "\n%J " + (obj.journal.title || obj.journal.titleCn);
             } else {
-                obj.authors.forEach(function (author) {
-                    text += "%A " + (author.given.cn || author.given.en) + ", " + (author.surname.cn || author.surname.en) + "\n";
-                });
+                if(!_.isEmpty(obj.authors)) {
+                    obj.authors.forEach(function (author) {
+                        text += "%A " + (author.given.cn || author.given.en) + ", " + (author.surname.cn || author.surname.en) + "\n";
+                    });
+                }
                 text += "%T " + (obj.title.cn || obj.title.en) + "\n%D " + obj.year + "\n%J " + (obj.journal.titleCn || obj.journal.title);
             }
             text += "\n%V " + obj.volume + "\n%N " + obj.issue + "\n%P " + (obj.elocationId || "") + "\n%R doi:http://dx.doi.org/" + obj.doi + "\n";
@@ -134,14 +146,18 @@ Router.map(function () {
             var name1 = "publisher/" + publisher.name + "/journal/" + obj.journal.title + "/" + obj.volume + "/" + obj.issue + "/" + obj.doi;
             var text = "RT Journal Article\nSR Electronic(1)\n";
             if (obj.language == 1) {
-                obj.authors.forEach(function (author) {
-                    text += "A1 " + (author.given.en || author.given.cn) + ", " + (author.surname.en || author.surname.cn) + "\n";
-                });
+                if(!_.isEmpty(obj.authors)) {
+                    obj.authors.forEach(function (author) {
+                        text += "A1 " + (author.given.en || author.given.cn) + ", " + (author.surname.en || author.surname.cn) + "\n";
+                    });
+                }
                 text += "YR " + obj.year + "\nT1 " + (obj.title.en || obj.title.cn) + "\nJF " + (obj.journal.title || obj.journal.titleCn);
             } else {
-                obj.authors.forEach(function (author) {
-                    text += "A1 " + (author.given.cn || author.given.en) + ", " + (author.surname.cn || author.surname.en) + "\n";
-                });
+                if(!_.isEmpty(obj.authors)) {
+                    obj.authors.forEach(function (author) {
+                        text += "A1 " + (author.given.cn || author.given.en) + ", " + (author.surname.cn || author.surname.en) + "\n";
+                    });
+                }
                 text += "YR " + obj.year + "\nT1 " + (obj.title.cn || obj.title.en) + "\nJF " + (obj.journal.titleCn || obj.journal.title);
             }
             text += "\nVO " + obj.volume + "\nIS " + obj.issue + "\nSP " + (obj.elocationId || "") + "\nOP " + (obj.startPage ? obj.startPage + "-" + obj.endPage : "") + "\nDO http://dx.doi.org/" + obj.doi + "\nUL " + Config.rootUrl + name1 + "\n\n";
@@ -185,13 +201,17 @@ Router.map(function () {
                 });
             }
             if (obj.language == 1) {
-                obj.authors.forEach(function (author) {
-                    text += "%A " + (author.given.en || author.given.cn) + ", " + (author.surname.en || author.surname.cn) + "\n";
-                });
+                if(!_.isEmpty(obj.authors)) {
+                    obj.authors.forEach(function (author) {
+                        text += "%A " + (author.given.en || author.given.cn) + ", " + (author.surname.en || author.surname.cn) + "\n";
+                    });
+                }
             } else {
-                obj.authors.forEach(function (author) {
-                    text += "%A " + (author.given.cn || author.given.en) + ", " + (author.surname.cn || author.surname.en) + "\n";
-                });
+                if(!_.isEmpty(obj.authors)) {
+                    obj.authors.forEach(function (author) {
+                        text += "%A " + (author.given.cn || author.given.en) + ", " + (author.surname.cn || author.surname.en) + "\n";
+                    });
+                }
             }
             text += "%P " + (obj.startPage ? obj.startPage + "-" + obj.endPage : "") + "\n%G ";
             if (obj.language == 1) {
