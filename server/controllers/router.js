debb9484117bf5acf0589eebdd93ac8bdcc0db5a
蒋凯 <kai.jiang@digitalpublishing.cn>
2016-06-16 17:11:47 +0800

    修复pdf下载，作者链接的错误

--- server/controllers/router.js
+++ server/controllers/router.js
@@ -307,7 +307,7 @@ Router.map(function () {
                 data.authors = _.map(article.authors, function (author) {
                     return {
                         name:getContentByLanguage(author.fullname, lang),
-                        authorUrl:Config.rootUrl + 'search?fq={"author":["\\"'+author.fullname + '\\""]}'
+                        authorUrl:Config.rootUrl + 'search?fq={"author":["\\"'+getContentByLanguage(author.fullname, lang) + '\\""]}'
                     };
                 });
             }
