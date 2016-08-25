000069fba157b70a56b7e096b825d0c5619ef263
蒋凯 <kai.jiang@digitalpublishing.cn>
2016-08-25 18:10:34 +0800

    shell and robots

--- restart-docker.sh
+++ restart-docker.sh
@@ -4,6 +4,9 @@
 echo “restarting docker container 正在重启服务器..”
 
 docker restart meteor
+docker restart meteor-2
+docker restart meteor-3
+docker restart meteor-4
 
 echo “exiting， update complete 更新完成了”
 
