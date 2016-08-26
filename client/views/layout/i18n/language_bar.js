02a809d8244da21593f5ff1a6c80ef012cd07f46
xiancui <451763649@qq.com>
2016-08-26 09:39:37 +0800

    小平台跳转+首页显示login

--- client/views/layout/i18n/language_bar.js
+++ client/views/layout/i18n/language_bar.js
@@ -8,7 +8,7 @@ Template.i18n_switch.events({
 
 Template.i18n_switch.helpers({
     miniPlatform : function(){
-        return Router.current().url.indexOf("miniplatform")>-1
+        return "www.scichina.com" || Router.current().url.indexOf("miniplatform")>-1
     }
 });
 
