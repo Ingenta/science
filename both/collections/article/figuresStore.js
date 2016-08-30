75607b3206e3fbcf36bbddfb957f2384f842decf
蒋凯 <kai.jiang@digitalpublishing.cn>
2016-08-30 14:36:02 +0800

    调整createThumb方法，较小的图片不进行缩放

--- both/collections/article/figuresStore.js
+++ both/collections/article/figuresStore.js
@@ -1,6 +1,33 @@
 var createThumb = function (fileObj, readStream, writeStream) {
     // Transform the image into a 10x10px thumbnail
-    gm(readStream, fileObj.name()).resize('600', '900').stream().pipe(writeStream);
+    //gm(readStream, fileObj.name()).resize('600', '900').stream().pipe(writeStream);
+    var maxThumbSize={width:600, height:900};
+    //console.log("fileObj's size is:" + fileObj.size());
+    gm(readStream, fileObj.name()).filesize({bufferStream: true},function(err,filesize){
+        if(err){
+            console.log(err.message);
+            return;
+        }
+        //console.log("readStream's size is:" + filesize);
+        this.size(function(err, value){
+            if(err){
+                console.log(err.message);
+                return;
+            }
+            //console.log("size is:" + value.width+"*"+value.height);
+            if(value.width>maxThumbSize.width || value.height>maxThumbSize.height){
+                this.resize(maxThumbSize.width,maxThumbSize.height)
+                //console.log("resize image");
+            }
+            this.stream(function(err, stdout, stderr){
+                if(err){
+                    console.log(err.message);
+                }
+                stdout.pipe(writeStream);
+            })
+        })
+    })
+
 };
 FiguresStore = new FS.Collection("figures", {
     stores: [new FS.Store.FileSystem("figures", {
