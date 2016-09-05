56f331b26fa8c7734935ee072d70ab230b000da7
蒋凯 <kai.jiang@digitalpublishing.cn>
2016-09-05 14:54:33 +0800

    Web of Science接口数据显示bug修复

--- client/views/publications/articles/amr.js
+++ client/views/publications/articles/amr.js
@@ -1,6 +1,6 @@
 Template.webOfScienceAMR.helpers({
     getData:function(){
         var article = Router.current().data();
-        return article && article.webOfScience;
+        return article && article.webOfScience && article.webOfScience.info;
     }
 })
