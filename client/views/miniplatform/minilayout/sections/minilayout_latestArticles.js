7661efaf60b88e22243e7277cba394b1cc12f586
xiancui <451763649@qq.com>
2016-09-22 09:03:58 +0800

    判断日期非空

--- client/views/miniplatform/minilayout/sections/minilayout_latestArticles.js
+++ client/views/miniplatform/minilayout/sections/minilayout_latestArticles.js
@@ -14,7 +14,7 @@ Template.layoutLatestArticles.helpers({
         var article = Articles.findOne({_id: Apid});
         if(article && article.published){
             if(_.isString(article.published))return article.published;
-            return article.published && article.published.format("yyyy-MM-dd");
+            return article.published && "(" + article.published.format("yyyy-MM-dd") + ")";
         }
     },
     articlesUrl: function(id){
