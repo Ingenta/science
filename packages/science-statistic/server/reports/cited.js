a87f463dc181f08e0e36d35684628d7c5852c0c8
xiancui <451763649@qq.com>
2016-10-14 18:40:55 +0800

    统计模版还原

--- packages/science-statistic/server/reports/cited.js
+++ packages/science-statistic/server/reports/cited.js
@@ -14,9 +14,9 @@ Science.Reports.getCitedArticleReportFile = function (query, fileName) {
     delete query.institutionId;
     delete query.action;
     query.citations = {$exists: true};
-    //var date = Articles.find(query,{sort: {'citationCount': -1}, limit: 100, fields: {title:1,doi:1,issue:1,volume:1,journal:1,publisher:1,citationCount:1}}).fetch();
-    var data = Science.Reports.getArticleCitedReportData(query);
-    var fields = Science.Reports.getCitedArticleReportFields(data.range);
+    var data = Articles.find(query,{sort: {'citationCount': -1}, limit: 100, fields: {title:1,doi:1,issue:1,volume:1,journal:1,publisher:1,citationCount:1}}).fetch();
+    //var data = Science.Reports.getArticleCitedReportData(query);
+    var fields = Science.Reports.getCitedArticleReportFields();
     return Excel.export(fileName, fields, data);
 };
 
@@ -68,44 +68,10 @@ Science.Reports.getCitedJournalReportFields = function (range) {
     return fields;
 };
 
-Science.Reports.getCitedArticleReportFields = function (range) {
+Science.Reports.getCitedArticleReportFields = function () {
     var fields = [
-        {
-            key: 'title',
-            title: '文章标题'
-        },
-        {
-            key: 'doi',
-            title: 'DOI',
-            width: 25
-        },
-        {
-            key: 'journal',
-            title: '期刊名称'
-        },
-        {
-            key: 'publisher',
-            title: '出版商',
-            width: 25
-        },
-        {
-            key: 'issue',
-            title: '期号',
-            width: 8
-        },
-        {
-            key: 'volume',
-            title: '卷号',
-            width: 8
-        },
-        {
-            key: 'total',
-            title: '被引用次数',
-            width: 15,
-            type: 'number'
-        }
         //{
-        //    key: 'title.cn',
+        //    key: 'title',
         //    title: '文章标题'
         //},
         //{
@@ -114,18 +80,13 @@ Science.Reports.getCitedArticleReportFields = function (range) {
         //    width: 25
         //},
         //{
-        //    key: 'journal.titleCn',
+        //    key: 'journal',
         //    title: '期刊名称'
         //},
         //{
         //    key: 'publisher',
         //    title: '出版商',
-        //    width: 25,
-        //    transform: function(val, doc) {
-        //        var publisher = Publishers.findOne({_id: val});
-        //        if(publisher)return publisher.chinesename;
-        //        return;
-        //    }
+        //    width: 25
         //},
         //{
         //    key: 'issue',
@@ -138,28 +99,67 @@ Science.Reports.getCitedArticleReportFields = function (range) {
         //    width: 8
         //},
         //{
-        //    key: 'citationCount',
+        //    key: 'total',
         //    title: '被引用次数',
         //    width: 15,
         //    type: 'number'
         //}
-    ];
-    var yearArr=[];
-    for(var i=range.max;i>=range.min;i--) {
-        yearArr.push(String(i));
-    }
-    _.each(yearArr,function(year){
-        fields.push({
-            key: 'years',
-            title: year,
-            width: 8,
-            type: 'number',
-            transform: function (val, doc) {
-                if(!val)return 0;
-                if(!val["year"+year])return 0;
-                return val["year"+year];
+        {
+            key: 'title.cn',
+            title: '文章标题'
+        },
+        {
+            key: 'doi',
+            title: 'DOI',
+            width: 25
+        },
+        {
+            key: 'journal.titleCn',
+            title: '期刊名称'
+        },
+        {
+            key: 'publisher',
+            title: '出版商',
+            width: 25,
+            transform: function(val, doc) {
+                var publisher = Publishers.findOne({_id: val});
+                if(publisher)return publisher.chinesename;
+                return;
             }
-        })
-    })
+        },
+        {
+            key: 'issue',
+            title: '期号',
+            width: 8
+        },
+        {
+            key: 'volume',
+            title: '卷号',
+            width: 8
+        },
+        {
+            key: 'citationCount',
+            title: '被引用次数',
+            width: 15,
+            type: 'number'
+        }
+    ];
+    //var yearArr=[];
+    //for(var i=range.max;i>=range.min;i--) {
+    //    yearArr.push(String(i));
+    //}
+    //_.each(yearArr,function(year){
+    //    fields.push({
+    //        key: 'years',
+    //        title: year,
+    //        width: 8,
+    //        type: 'number',
+    //        transform: function (val, doc) {
+    //            if(!val)return 0;
+    //            if(!val["year"+year])return 0;
+    //            return val["year"+year];
+    //        }
+    //    })
+    //})
     return fields;
 };
