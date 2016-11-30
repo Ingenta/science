3ac1b974fa39408d08c2753e2f26b3d1917eb938
xiancui <451763649@qq.com>
2016-11-30 13:32:33 +0800

    视频文件代码优化

--- server/publish/journal/medias.js
+++ server/publish/journal/medias.js
@@ -1,3 +1,20 @@
 Meteor.publish('medias', function() {
 	return Collections.Medias.find();
 });
+
+Meteor.publish('journalMediasInfo', function(journalShortTitle) {
+	if(journalShortTitle) {
+		check(journalShortTitle, String);
+		var journal = Publications.findOne({shortTitle: journalShortTitle});
+		if (!journal)return this.ready();
+		var journalId = journal._id;
+		return Collections.Medias.find({"journalId": journalId, doi: null});
+	}
+});
+
+Meteor.publish('articleMediasInfo', function(doi) {
+	if(doi) {
+		check(doi, String);
+		return Collections.Medias.find({doi: doi});
+	}
+});
