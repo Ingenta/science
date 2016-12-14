704dffc45f1c0b876569a023a090add3e66f359f
xiancui <451763649@qq.com>
2016-12-14 15:28:20 +0800

    点击展开功能

--- client/views/publications/articles/pacs.js
+++ client/views/publications/articles/pacs.js
@@ -7,4 +7,16 @@ Template.pacsTemplate.helpers({
 		};
 		return SolrQuery.makeUrl(option);
 	}
-})
+});
+
+Template.pacsTemplate.events({
+	'hide.bs.collapse .collapse':function(e,t){
+		e.stopPropagation();
+		$(e.currentTarget).siblings('p').find("i").removeClass("fa-minus").addClass("fa-plus");
+	},
+	'show.bs.collapse .collapse':function(e,t){
+		e.stopPropagation();
+		$(e.currentTarget).siblings('p').find("i").removeClass("fa-plus").addClass("fa-minus");
+	}
+});
+
