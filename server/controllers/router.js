fb55514453c3927b44e73deaadb145c252b933ec
xiancui <451763649@qq.com>
2017-06-15 11:54:38 +0800

    完备搜索引擎数据

--- server/controllers/router.js
+++ server/controllers/router.js
@@ -14,7 +14,21 @@ Router.map(function () {
     }).get(function(){
       var article = Articles.findOne({doi: this.params.publisherDoi + "/" + this.params.articleDoi.replace(/-slash-/g,"/")});
       var title = article.title.en || article.title.cn;
-      var description = article.abstract.en || article.abstract.cn;
+      var description = "";
+      if(article.abstract){
+          description = article.abstract.en || article.abstract.cn;
+      }
+      var publisher = Publishers.findOne({_id:article.publisher});
+      var publisherName = "";
+      if(publisher){
+          publisherName = publisher.name || publisher.chinesename;
+      }
+      var journal = Publications.findOne({_id:article.journalId});
+      var journalName = "";
+      if(journal){
+          journalName = journal.title || journal.titleCn;
+      }
+      var firstPage = article.startPage || article.elocationId;
       var author = ''
       if(!_.isEmpty(article.authors)){
           var authorsArr = []
@@ -28,10 +42,10 @@ Router.map(function () {
         '<!DOCTYPE html>\n'
         +'<html>\n'
         +'  <head>\n'
-        +'    <link rel="stylesheet" type="text/css" class="__meteor-css__" href="/b1507784cc951fd05883029bbe12562ca74ab551.css?meteor_css_resource=true">  <link rel="stylesheet" type="text/css" class="__meteor-css__" href="/d3787456fd39465964aa7d2088a9ee3ba1fdbe47.css?meteor_css_resource=true">\n'
-        +'    <script type="text/javascript">__meteor_runtime_config__ = JSON.parse(decodeURIComponent("%7B%22meteorRelease%22%3A%22METEOR%401.2.1%22%2C%22PUBLIC_SETTINGS%22%3A%7B%7D%2C%22ROOT_URL%22%3A%22http%3A%2F%2Fengine.scichina.com%22%2C%22ROOT_URL_PATH_PREFIX%22%3A%22%22%2C%22autoupdateVersion%22%3A%227c20b022ee6533a292bb49e45bfc10729f6ab3ed%22%2C%22autoupdateVersionRefreshable%22%3A%225ad99d377c38b01b6dc0ab7402502ec8760910ff%22%2C%22autoupdateVersionCordova%22%3A%22none%22%7D"));</script>\n'
-        +'    <script type="text/javascript" src="/1b1920021f39c46022c75a10b71287cf5490b560.js?meteor_js_resource=true"></script>\n'
-        +'    <script type="text/javascript" src="/redirct.js"></script>\n\n'
+        +'    <link rel="stylesheet" type="text/css" class="__meteor-css__" href="/b1507784cc951fd05883029bbe12562ca74ab551.css?meteor_css_resource=true">  <link rel="stylesheet" type="text/css" class="__meteor-css__" href="/d3787456fd39465964aa7d2088a9ee3ba1fdbe47.css?meteor_css_resource=true">\n\n'
+        +'    <script type="text/javascript">__meteor_runtime_config__ = JSON.parse(decodeURIComponent("%7B%22meteorRelease%22%3A%22METEOR%401.2.1%22%2C%22PUBLIC_SETTINGS%22%3A%7B%7D%2C%22ROOT_URL%22%3A%22http%3A%2F%2Fengine.scichina.com%22%2C%22ROOT_URL_PATH_PREFIX%22%3A%22%22%2C%22autoupdateVersion%22%3A%227c20b022ee6533a292bb49e45bfc10729f6ab3ed%22%2C%22autoupdateVersionRefreshable%22%3A%225ad99d377c38b01b6dc0ab7402502ec8760910ff%22%2C%22autoupdateVersionCordova%22%3A%22none%22%7D"));</script>\n\n'
+        +'    <script type="text/javascript" src="/1b1920021f39c46022c75a10b71287cf5490b560.js?meteor_js_resource=true"></script>\n\n'
+        +'    <script type="text/javascript" src="/redirct.js"></script>\n\n\n'
         +'    <meta name="fragment" content="!">\n'
         +'    <meta charset="utf-8">\n'
         +'    <meta http-equiv= "X-UA-Compatible" content = "IE=edge,chrome=1">\n'
@@ -44,10 +58,24 @@ Router.map(function () {
         +'         《中国科学》杂志社\n'
         +'    </title>\n'
         +'    <meta name="title" content="' + title + '">\n'
-        +'    <meta name="title" content="' + description + '">\n'
         +'    <meta name="author" content="' + author + '">\n'
+        +'    <meta name="description" content="' + description + '">\n'
+        +'    <meta name="citation_publisher" content="' + publisherName + '">\n'
+        +'    <meta name="citation_title" content="' + title + '">\n'
+        +'    <meta name="citation_date" content="' + article.published.format("yyyy/MM/dd") + '">\n'
+        +'    <meta name="citation_doi" content="' + article.doi + '">\n'
+        +'    <meta name="citation_abstract" content="' + description + '">\n'
+        +'    <meta name="citation_journal_title" content="' + journalName + '">\n'
+        +'    <meta name="citation_journal_abbrev" content="' + journal.abbrevTitle + '">\n'
+        +'    <meta name="citation_issn" content="' + journal.issn.slice(0, 4) + "-" + journal.issn.slice(4) + '">\n'
+        +'    <meta name="citation_volume" content="' + article.volume + '">\n'
+        +'    <meta name="citation_issue" content="' + article.issue + '">\n'
+        +'    <meta name="citation_firstpage" content="' + firstPage + '">\n'
+        +'    <meta name="citation_lastpage" content="' + article.endPage + '">\n'
+        +'    <meta name="citation_abstract_html_url" content="' + window.location.href + '">\n'
+        +'    <meta name="citation_pdf_url" content="' + window.location.origin+"/downloadPdf/"+article._id + '">\n'
         +'  </head>\n'
-        +'  <body>\n'
+        +'  <body>\n\n'
         +'  </body>\n'
         +'</html>'
       )
