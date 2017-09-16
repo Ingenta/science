883f14cbb365740625d13d8621922a176ac6ebdb
蒋凯 <kai.jiang@digitalpublishing.cn>
2017-09-16 17:23:23 +0800

    set publish name

--- packages/science-permissions/publication.js
+++ packages/science-permissions/publication.js
@@ -1,4 +1,4 @@
-Meteor.publish(null, function() {                                              // 1
+Meteor.publish("getUserRoles", function() {                                              // 1
 	return Permissions.userCan("get-users-roles","permissions",this.userId) ? Meteor.users.find({}, {fields: {profile: 1, orbit_roles: 1, emails: 1}}) : this.ready();
 	//return Meteor.users.find({});
-}); 
+});
