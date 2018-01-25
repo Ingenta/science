9fdddad68e013e62d6f92b77d864c6f51e62fab1
xiancui <451763649@qq.com>
2018-01-25 11:15:12 +0800

    阅读行为统计日志打印

--- packages/science-auto-task/server/cronTasks/articleMetricsCount.js
+++ packages/science-auto-task/server/cronTasks/articleMetricsCount.js
@@ -4,10 +4,13 @@
          return parser.text(Config.AutoTasks.articleMetrics.rate || "at 1:30 am on Sat");//默认每周凌晨1点半检查一次
      },
      job: function () {
-         var article = Articles.find({}, {fields: {doi: 1}});
-         if(article){
+         var index = 0;
+         var article = Articles.find({}, {fields: {doi: 1}}).fetch();
+         if(!_.isEmpty(article)){
              article.forEach(function (article) {
                  if (!article || !article._id)return;
+                 logger.info("Article to start report");
+                 index++;
                  Meteor.call("getArticlePageViewsPieChartData", article._id, function (err, response) {
                      if(_.isEmpty(response))return this.ready();
                      var count = MetricsCount.find({articleId:article._id}, {fields: {_id: 1}}).fetch();
@@ -16,12 +19,14 @@
                              MetricsCount.remove({_id:item._id});
                          });
                      }
+                     logger.info("Article to remove history Aid= "+article._id);
                      MetricsCount.insert({
                          articleId: article._id,
                          dataCount: response,
                          type: "1",
                          createDate:new Date()
                      });
+                     logger.info("ArticlePageViews to insert P"+index);
                  });
                  Meteor.call("getArticlePageLocationReport", "fulltext", article._id, function (err, arr) {
                      MetricsCount.insert({
@@ -30,6 +35,7 @@
                          type: "2",
                          createDate:new Date()
                      });
+                     logger.info("ArticlePageLocation to insert P"+index);
                  });
                  Meteor.call("getArticlePageViewsGraphData", article._id, function (err, response) {
                      if(_.isEmpty(response))return this.ready();
@@ -39,6 +45,7 @@
                          type: "3",
                          createDate:new Date()
                      });
+                     logger.info("ArticlePageViewsGraph to insert P"+index);
                  });
              });
          }
